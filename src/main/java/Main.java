bccc22a6d622e788ef5b9b1c6b254504b51e0d57
Jiwon Bae <jwb8705@gmail.com>
2017-10-25 16:43:01 +0900

    Chaged the location of code for creating instances

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,14 @@ import java.util.ArrayList;
 
 public class Main {
 	public static void main(String[] args) {
-		InputView inputView = new InputView();
-		ResultView resultView = new ResultView();
 		StringProcess stringProcess = new StringProcess();
 		System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+		InputView inputView = new InputView();
 		String carNamesString = inputView.getCarNamesFromUser();
 		ArrayList<String> carNames = stringProcess.parseInputString(carNamesString);
 		System.out.println("시도할 회수는 몇 회 인가요?");
 		int numOfTry = inputView.getNumOfTry();
+		ResultView resultView = new ResultView();
 		resultView.printResult(carNames, numOfTry);
 		
 	}
