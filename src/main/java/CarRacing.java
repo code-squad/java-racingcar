f4d3873b5b0c81c58031f9ef831ef85d20e23dd3
Will Park <31429900+JongsooPark1@users.noreply.github.com>
2017-11-16 13:32:25 +0900

    [Will] CarRacing 첫 PR 보내겠습니다 (#15)
    * Add CarRacing.java

---
+++ src/main/java/CarRacing.java
@@ -0,0 +1,36 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class CarRacing {
+	private int time;
+	private int[] carPositions = {1, 1, 1};
+	
+	public void run() {
+		Random random = new Random();
+		String[] goMark = {"-", "-", "-"};
+		for (int i = 0; i < carPositions.length; i++) {
+			int randomNum = random.nextInt(10);
+			if (randomNum >= 4) {
+				carPositions[i] += 1;
+			}
+			for (int j = 0; j < carPositions[j]; j++) {
+				goMark[i] += "-";
+				System.out.println(goMark[i]);
+			}
+		}	
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("자동차 대수는 몇 대 인가요?");
+		Scanner scanner = new Scanner(System.in);
+		int carNum = scanner.nextInt();
+		System.out.println("시도할 회수는 몇 회 인가요?");
+		CarRacing carRacing = new CarRacing();
+		carRacing.time = scanner.nextInt();
+		int timeNum = carRacing.time;
+		for (int i = 0; i < timeNum; i++) {
+			carRacing.run();
+		}
+	}
+}
