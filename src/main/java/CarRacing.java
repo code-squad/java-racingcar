b16e0080c58baece9ce688af5ea9410b8c297006
Will Park <31429900+JongsooPark1@users.noreply.github.com>
2017-11-20 16:54:38 +0900

    [Will] Racingcar step2 (#18)
    * Remodify CarRacing-step1
    
    * Modify CarRacing.java for step2
    
    * Replace fields with  local variables in CarRacing.java
    
    * Replace instance methods with static methods in CarRacing.java
    
    * Change branch master to jongsoopark1

--- src/main/java/CarRacing.java
+++ src/main/java/CarRacing.java
@@ -2,35 +2,82 @@
 import java.util.Random;
 import java.util.Scanner;
 
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
 public class CarRacing {
-	private int time;
-	private int[] carPositions = {1, 1, 1};
+	public static String[] getNames() {
+		System.out.println("이름을 입력하세요(ex : will,pobi,honux)");
+		Scanner sc = new Scanner(System.in);
+		String temp = sc.nextLine();
+		String[] names = temp.split(",");
+		return names;
+	}
 	
-	public void run() {
+	public static int[] getCarPositions(String[] names) {
+		int[] carPositions = new int[names.length];
+		for (int i = 0; i < names.length; i++) {
+			carPositions[i] = 1;
+		}
+		return carPositions;
+	}
+	
+	public static int getTime() {
+		System.out.println("시도할 회수는 몇 회인가요?");
+		Scanner sc = new Scanner(System.in);
+		int temp = sc.nextInt();
+		if (!(temp > 0)) {
+			System.out.println("양의 정수로 다시 입력하세요");
+			getTime();
+			return -1;
+		}
+		return temp;
+	}
+	
+	public static int makeRandomNum() {
 		Random random = new Random();
-		String[] goMark = {"-", "-", "-"};
+		int randomNum = random.nextInt(10);
+		if (randomNum >= 4) {
+			return 1;
+		}
+		return 0;
+	}
+	
+	public static int[] run(int[] carPositions) {
 		for (int i = 0; i < carPositions.length; i++) {
-			int randomNum = random.nextInt(10);
-			if (randomNum >= 4) {
-				carPositions[i] += 1;
-			}
-			for (int j = 0; j < carPositions[j]; j++) {
-				goMark[i] += "-";
-				System.out.println(goMark[i]);
-			}
-		}	
+			carPositions[i] += makeRandomNum();
+		}
+		return carPositions;
+	}
+	
+	public static String[] makeMarkDash(int[] carPositions, int j) {
+		String[] markDash = new String[carPositions.length];
+		for (int i = 0; i < carPositions.length; i++) {
+			markDash[i] = "-";
+		}
+		for (int i = 0; i < carPositions[j] - 1; i++) {
+			markDash[j] += "-";
+		}
+		return markDash;
+	}
+	
+	public static void show(int[] carPositions, String[] names) {
+		run(carPositions);
+		for (int i = 0; i < carPositions.length; i++) {
+			System.out.println(names[i] + " : " + makeMarkDash(carPositions, i)[i]);
+		}
+	}
+	
+	public static void showNTimes(int times, int[] carPositions, String[] names) {
+		for (int i = 0; i < times; i++) {
+			show(carPositions, names);
+			System.out.println("");
+		}
 	}
 	
 	public static void main(String[] args) {
-		System.out.println("자동차 대수는 몇 대 인가요?");
-		Scanner scanner = new Scanner(System.in);
-		int carNum = scanner.nextInt();
-		System.out.println("시도할 회수는 몇 회 인가요?");
-		CarRacing carRacing = new CarRacing();
-		carRacing.time = scanner.nextInt();
-		int timeNum = carRacing.time;
-		for (int i = 0; i < timeNum; i++) {
-			carRacing.run();
-		}
+		String[] names = getNames();
+		int times = getTime();
+		int[] carPositions = getCarPositions(names);
+		showNTimes(times, carPositions, names);
 	}
 }
