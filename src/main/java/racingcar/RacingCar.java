e9dfaaa949f19c78555ae28c907ca109fe52cafc
ksm0814 <ksm0814@naver.com>
2017-12-01 16:08:31 +0900

    1단계 1차 수정

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -6,53 +6,56 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class RacingCar {
-	private static List<Integer> car_pos;
 
-	static List<Boolean> isMove;
-	static int car_num;
-	static int try_num;
-
-	static Random ran;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		car_pos = new ArrayList<Integer>();
+		List<Integer> carPosition = new ArrayList<Integer>();
 
 		System.out.println("자동차 대수는 몇 대 인가요?");
-		car_num = sc.nextInt();
+		int carNumber = sc.nextInt();
 		System.out.println("시도할 횟수는 몇 회 인가요?");
-		try_num = sc.nextInt();
+		int tryNumber = sc.nextInt();
 
-		setCar();
-		goStop();
-		car_pos.clear();
+		setCar(carNumber, carPosition);
+		goStop(carNumber, tryNumber, carPosition);
+		carPosition.clear();
 	}
 
-	public static void setCar() {
-		for (int i = 0; i < car_num; i++) {
-			car_pos.add(1);
-		}
-
-	}
-
-	public static void goStop() {
-		ran = new Random();
-		for (int i = 0; i < try_num; i++) {
-			for (int k = 0; k < car_num; k++) {
-				int j = ran.nextInt(10);
-				if (j >= 4)
-					car_pos.set(k, car_pos.get(k) + 1);
-			}
-			drawStatus();
-			System.out.println("\n\n");
+	public static void setCar(int carNumber, List<Integer> carPosition) {
+		for (int i = 0; i < carNumber; i++) {
+			carPosition.add(1);
 		}
 	}
 
-	public static void drawStatus() {
-		for (int i = 0; i < car_num; i++) {
-			for (int t = car_pos.get(i); t > 0; t--)
-				System.out.print("-");
-			System.out.println();
+	public static void goStop(int carNumber, int tryNumber, List<Integer> carPosition) {
+		Random ran = new Random();
+		for (int i = 0; i < tryNumber; i++)
+			moveCar(carNumber, ran, carPosition);
+
+	}
+
+	private static void moveCar(int carNumber, Random ran, List<Integer> carPosition) {
+		for (int k = 0; k < carNumber; k++) {
+			int j = ran.nextInt(10);
+			if (j >= 4)
+				carPosition.set(k, carPosition.get(k) + 1);
 		}
+		drawStatus(carNumber, carPosition);
+		System.out.println("\n\n");
+
+	}
+
+	public static void drawStatus(int carNumber, List<Integer> carPosition) {
+		for (int i = 0; i < carNumber; i++) {
+			drawCar(i, carPosition);
+
+		}
+	}
+
+	private static void drawCar(int i, List<Integer> carPosition) {
+		for (int t = carPosition.get(i); t > 0; t--)
+			System.out.print("-");
+		System.out.println();
 	}
 }
