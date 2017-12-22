eceae6160bb3a17aca8cba63e64bbc1ddafe2850
Abel ko <esung1129@gmail.com>
2017-12-22 16:34:19 +0900

    Racing car level1구현 (#38)
    * Racing car level1구현
    
    * RacingCar level 1구현(리팩토링1)
    
    makeCar()메소드 안에서 배열을 생성함
    ForwordCondition 변수 앞글자를 소문자로 변경함

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,5 +1,69 @@
 package racingcar;
 
-public class RacingCar {
+import java.util.Scanner;
+import java.util.Random;
 
+public class RacingCar {
+	
+	private static void print(String sentence) {
+		System.out.println(sentence);
+	}
+	
+	private static int getInt(String sentence, Scanner sc) {
+		print(sentence);
+		return sc.nextInt();
+	}
+	
+	private static String[] makeCar(String carDistance, int carCount) {
+		String[] car = new String[carCount]; // 자동차 개수만큼의 크기의 배열 생성;
+		for (int i = 0; i < carCount; i++) {
+			car[i] = carDistance;
+			System.out.println(car[i]);
+		}
+		return car;
+	}
+	
+	private static int makeRandomNumber() {
+		Random rd = new Random();
+		return rd.nextInt(10);
+	}
+	
+	private static String determineCondition(String[] car, String carDistance, int i) {
+		int forwardCondition = makeRandomNumber();// 4이상이면 이동
+		if (forwardCondition >= 4) {
+			car[i] += carDistance;
+		}
+		return car[i];
+	}
+	
+	public static void moveCar(String carDistance, int carCount, String[] car) {
+		for (int i = 0; i < carCount; i++) { 
+			print(determineCondition(car, carDistance, i));// 자동차
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int carCount = getInt("자동차 수 : ", sc);
+		int moveCount = getInt("이동 회수 : ", sc);
+
+		print("==== 시작 ====");
+		
+		String carDistance = "-";
+		// 자동차 개수 생성
+		String[] cars = makeCar(carDistance, carCount);
+		
+		print("==== 결과 ====");
+
+		// 자동차들이 이동 횟수만큼 이동
+		// 자동차 개수 만들고, 1번째자동차에 이동조건을 걸고, 맞으면 이동
+		for (int i = 0; i < moveCount; i++) {
+			System.out.println((i+1) + "회 움직였습니다." );
+			moveCar(carDistance, carCount, cars);
+			System.out.println();
+		}
+
+	}
 }
