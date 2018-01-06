3b1685af7739d5074291eb502c25f53d8bc46cec
kjs4395 <kjs4395@gmail.com>
2018-01-06 17:36:39 +0900

    자동차게임 step 1

---
+++ src/test/java/racingGame/RacingGameTest.java
@@ -0,0 +1,24 @@
+package racingGame;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class RacingGameTest {
+
+    private RacingGame racingGame;
+
+    @Before
+    public void setUp() {
+        racingGame = new RacingGame(3,5);
+    }
+
+    @Test
+    public void isAdvanceTest() throws Exception {
+    }
+
+
+}
