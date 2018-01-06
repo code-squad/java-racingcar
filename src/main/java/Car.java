2c8342aa7af1d97348455688913a76fc3ee53f0a
Moonchan <moonchan@smartauth.kr>
2018-01-06 15:46:22 +0900

    first commit

---
+++ src/main/java/Car.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Moonchan on 2018. 1. 6..
+ */
+public class Car {
+}
---
+++ src/test/java/CarTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Moonchan on 2018. 1. 6..
+ */
+public class CarTest {
+
+}
