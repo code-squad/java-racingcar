57681903a1dee61756fdc7cd3cc2500e2ba0e08c
youngwoolee <leeyw910205@gmail.com>
2018-01-06 15:52:00 +0900

    intial commit (#53)

---
+++ src/test/java/CarTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Joeylee on 2018-01-06.
+ */
+public class CarTest {
+
+    @Test
+    public int test() {
+
+    }
+
+}
