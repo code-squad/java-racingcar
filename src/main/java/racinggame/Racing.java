53e7e85df4d3253fb522f97b0e0199a87af522d3
Sooyoung Park <sooyoung32@gmail.com>
2018-01-07 12:12:14 +0900

    [mod] 이중 for loop을 자바 8로 변경해 indent 를 줄임

--- src/main/java/racinggame/Racing.java
+++ src/main/java/racinggame/Racing.java
@@ -3,6 +3,7 @@ package racinggame;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.IntStream;
 
 public class Racing {
     private List<Car> cars;
@@ -23,16 +24,12 @@ public class Racing {
     }
 
     public void racing() {
-        for (int i = 0 ; i < count ; i++) {
-            for (Car car : cars) {
-                car.move(new Random().nextInt(10));
-            }
-        }
+        IntStream.range(0,count).forEachOrdered(i -> cars.forEach(car -> car.move(new Random().nextInt(10))));
     }
 
     public void racingResult() {
         for (Car car : cars) {
-            car.printPosition();
+            car.printResult();
         }
     }
 }
