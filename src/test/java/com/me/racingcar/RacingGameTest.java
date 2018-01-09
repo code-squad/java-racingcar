e9f54bd6cd67ba7a3eca80ae59232416225d8ed9
ttaeyoung <ttaeyoung@gmail.com>
2018-01-09 11:17:41 +0900

    test(step1): Car 테스트 함수 추가

--- src/test/java/com/me/racingcar/RacingGameTest.java
+++ src/test/java/com/me/racingcar/RacingGameTest.java
@@ -28,7 +28,4 @@ public class RacingGameTest {
         RacingGame.start(10, 0);
     }
 
-
-
-
 }
--- src/test/java/com/me/racingcar/car/CarTest.java
+++ src/test/java/com/me/racingcar/car/CarTest.java
@@ -3,6 +3,8 @@ package com.me.racingcar.car;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Random;
+
 import static org.junit.Assert.*;
 
 public class CarTest {
@@ -23,6 +25,17 @@ public class CarTest {
     public void move_value_ge_4(){
         int curPosition = car.move(4);
         assertTrue(0 < curPosition);
+    }
+
+    @Test
+    public void printPosition(){
+        Random random = new Random();
+        int cnt = 100, position=0;
+        for(int idx=0 ; idx<cnt ; idx++){
+            position = car.move(random.nextInt(10));
+        }
+
+        assertTrue(position == car.printPosition().length());
     }
 
 }
