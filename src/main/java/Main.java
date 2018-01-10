b28c80b9e63234fbdc8ba11c9c7cba3c0075b24a
ryudung <rhkdgus221@naver.com>
2018-01-10 18:06:58 +0900

    coflict 풀려라...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,14 @@
 import In.Input;
 import car.Race;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Race race = new Race(Input.inputCarSize(), Input.inputTryNumber(), 9, 4);
-        race.moveCars();
-        race.printCars();
+        List<String> carNames = Arrays.asList(Input.inputCars().split(","));
+        Race race = new Race(carNames, Input.inputTryNumber(), 9, 4);
+        race.startRacing();
+        race.printRacing();
     }
 }
--- src/test/java/car/CarTest.java
+++ src/test/java/car/CarTest.java
@@ -4,6 +4,8 @@ package car;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.IntStream;
 
 import static org.junit.Assert.assertEquals;
@@ -13,10 +15,16 @@ import static org.junit.Assert.assertTrue;
 public class CarTest {
 
     private Race race;
+    private List carNames;
 
     @Before
     public void init(){
-        race = new Race(3, 5,9,4);
+        carNames = new ArrayList(){{
+            add("류광현차");
+            add("조정훈차");
+            add("강지선차");
+        }};
+        race = new Race(carNames, 5,9,4);
     }
 
     @Test
@@ -42,14 +50,44 @@ public class CarTest {
 
     @Test
     public void createCarBySize_테스트() {
-        race = new Race(3, 5,9,4);
+        race = new Race(carNames, 5,9,4);
         assertEquals(race.getCars().size(), 3);
     }
 
     @Test
     public void move_plus_1_테스트(){
-        Car car = new Car();
+        Car car = new Car("pobi",0);
         car.move(1);
         assertEquals(car.getPosition(), 1);
+    }
+
+    @Test
+    public void calCulateWinners_우승자1명_테스트(){
+        ArrayList cars = new ArrayList(){{
+            add(new Car("car1",1));
+            add(new Car("car2",3));
+            add(new Car("car3",2));
+        }};
+        List<Car> winners = race.calCulateWinners(cars, 3);
+        assertEquals("car2", winners.get(0).getName());
+        assertEquals(3, winners.get(0).getPosition());
+    }
+
+    @Test
+    public void calCulateWinners_우승자여러명_테스트(){
+        ArrayList cars = new ArrayList(){{
+            add(new Car("car1",3));
+            add(new Car("car2",3));
+            add(new Car("car3",3));
+        }};
+        List<Car> winners = race.calCulateWinners(cars, 3);
+
+        assertEquals("car1", winners.get(0).getName());
+        assertEquals(3, winners.get(0).getPosition());
+        assertEquals("car2", winners.get(1).getName());
+        assertEquals(3, winners.get(1).getPosition());
+        assertEquals("car3", winners.get(2).getName());
+        assertEquals(3, winners.get(2).getPosition());
+
     }
 }
