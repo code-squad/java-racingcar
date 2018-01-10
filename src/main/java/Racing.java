da43d4d6288ac9e14e79b58f183ff7c111476e5d
jimmy <foodsksms@gmail.com>
2018-01-10 16:07:30 +0900

    Finish Racing step1

--- src/main/java/Racing.java
+++ src/main/java/Racing.java
@@ -2,11 +2,11 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Racing {
-    private int time;
-    //    static private int [] carPositions = new int[number];
+    static private int time;
+    //static private int [] carPositions = new int[number];
     static private int [] carPositions;
 
-    public String repeat (int number) {
+    public static String repeat (int number) {
         String string = "";
         for (int i = 0; i < number; i++) {
             string += "-";
@@ -14,17 +14,18 @@ public class Racing {
         return string;
     }
 
-    public void print(int [] carPositions) {
+    public static void print_result(int [] carPositions) {
         for (int i = 0; i < carPositions.length; i++) {
             int empty = carPositions[i];
+
             String result = repeat(empty);
+
             System.out.println(result);
         }
     }
 
     public static void main (String args[]) {
         Racing racing = new Racing();
-
         Random rnd = new Random();
         Scanner scanner = new Scanner(System.in);
 
@@ -32,20 +33,21 @@ public class Racing {
         String car_number = scanner.nextLine();
         int number = Integer.parseInt(car_number);
         carPositions = new int[number];
-        // 배열을 초기화한다.
 
         System.out.println("시도할 회수는 몇 회 인가요?");
         String count = scanner.nextLine();
         int counting = Integer.parseInt(count);
 
         System.out.println("실행 결과");
+
         for (int i = 0; i < number; i++) {
             for (int j = 0; j < counting; j++) {
                 if (rnd.nextInt(9) >= 4) {
                     carPositions[i] += 1;
                 }
             }
-            System.out.println(carPositions[i]);
+            //System.out.println(i+"번째 자동차 " + carPositions[i]);
         }
+        print_result(carPositions);
     }
 }
