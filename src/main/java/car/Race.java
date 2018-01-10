c7cd2c7ba908f75c999ddb3daa1995566a75141e
ryudung <rhkdgus221@naver.com>
2018-01-10 13:45:49 +0900

    [step2] 2

--- src/main/java/car/Race.java
+++ src/main/java/car/Race.java
@@ -1,7 +1,7 @@
 package car;
 
-import out.Output;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -13,23 +13,51 @@ public class Race {
     private int isMovablesRange;
     private int isMovablesStandard;
 
-    public Race(int carSize, int tryNumber, int isMovablesRange, int isMovablesStandard) {
-        this.cars = createCarBySize(carSize);
+    public Race(List<String> carNames, int tryNumber, int isMovablesRange, int isMovablesStandard) {
+        this.cars = createCars(carNames);
         this.tryNumber = tryNumber;
         this.isMovablesRange = isMovablesRange;
         this.isMovablesStandard = isMovablesStandard;
     }
 
-    public void moveCars() {
-        IntStream.range(0, this.tryNumber).forEach(i -> cars.forEach(car -> car.move(CarMoveCalculator.calculatePosition(isMovablesRange, isMovablesStandard))));
+    private void moveCars(List<Car> cars){
+        cars.forEach(car -> car.move(CarMoveCalculator.calculatePosition(isMovablesRange, isMovablesStandard)));
     }
 
-    private List<Car> createCarBySize(int carSize) {
-        return IntStream.range(0, carSize).mapToObj(i -> new Car()).collect(Collectors.toList());
+    public void startRacing() {
+        IntStream.range(0, this.tryNumber).forEach(i -> moveCars(cars));
     }
 
-    public void printCars() {
-        cars.forEach(car -> Output.printMove(car.getPosition()));
+    public List<Car> calCulateWinners(List<Car> cars, int maxPosition){
+        return cars.stream().filter(car->car.getPosition() == maxPosition).collect(Collectors.toList());
+    }
+
+    private Car getMaxPositionCar(List<Car> cars) {
+        return cars.stream().max(Car::compareTo).orElse(new Car("",0));
+    }
+
+    private List<Car> createCars(List<String> carNames) {
+        List<Car> cars = new ArrayList<>();
+        carNames.forEach(carName->cars.add(new Car(carName,0)));
+        return cars;
+    }
+
+    public void printRacing(){
+        printCarsMove();
+        printWinner();
+    }
+
+    private void printCarsMove() {
+        cars.forEach(car -> System.out.println(car.toString()));
+    }
+
+    private void printWinner() {
+        System.out.println("");
+        for(Car car: calCulateWinners(this.cars, getMaxPositionCar(cars).getPosition())){
+            System.out.print(car.getName() + " ");
+        }
+        System.out.print("가 최종 우승하셨습니다");
+        System.out.println("");
     }
 
     public List<Car> getCars() {
