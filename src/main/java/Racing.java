3a4824e6e5acb756a0c5326531ac863043bb208d
jimmy <foodsksms@gmail.com>
2018-01-11 14:37:24 +0900

    Divide main method

--- src/main/java/Racing.java
+++ src/main/java/Racing.java
@@ -1,14 +1,11 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.util.Random;
 import java.util.Scanner;
 
 public class Racing {
     private static final Logger logger = LoggerFactory.getLogger(Racing.class);
-    //private static final Logger log = LoggerFactory.getLogger(Racing.class);
-    // getLogger()에 전달하는 class이름이 중요. 이걸편안하게 하기위해 템플릿을 사용
-    static private int time;
+    static private int count, carnumber;
     static private int [] carPositions;
 
     public static String repeat (int number) {
@@ -29,34 +26,40 @@ public class Racing {
         }
     }
 
+    public static int [] Carinput(Scanner scanner){
+        logger.info("자동차 대수는 몇 대 인가요? -- 메서드 분리");
+        int car_number = scanner.nextInt();
+        carPositions = new int[car_number];
+        return carPositions;
+    }
+
+    public static int Count(Scanner scanner){
+        logger.info("시도할 회수는 몇 회 인가요?");
+        int count = scanner.nextInt();
+        return count;
+    }
+
+    public static void loop(int carnumber,int count){
+        Random rnd = new Random();
+        for (int i = 0; i < carnumber;i++) {
+            for (int j = 0; j < count; j++) {
+                if (rnd.nextInt(9) >=4){
+                    carPositions[i] += 1;
+                }
+            }
+        }
+        print_result(carPositions);
+    }
+
     public static void main (String args[]) {
         Racing racing = new Racing();
         Random rnd = new Random();
         Scanner scanner = new Scanner(System.in);
+        Carinput(scanner);
+        count = Count(scanner);
+        carnumber = carPositions.length;
+        logger.info("실행 결과");
+        loop(carnumber, count);
 
-        //System.out.println("자동차 대수는 몇 대 인가요?");
-        logger.info("자동차 대수는 몇 대 인가요? - using logger");
-        //log.debug("자동차 대수는 몇 대 인가요? - using logger");도 가능하다. log.level("message");
-        String car_number = scanner.nextLine();
-        int number = Integer.parseInt(car_number);
-        carPositions = new int[number];
-
-        //System.out.println("시도할 회수는 몇 회 인가요?");
-        logger.info("시도할 회수는 몇 회 인가요? - using logger");
-        String count = scanner.nextLine();
-        int counting = Integer.parseInt(count);
-
-        //System.out.println("실행 결과");
-        logger.info("실행 결과 - using logger");
-
-        for (int i = 0; i < number; i++) {
-            for (int j = 0; j < counting; j++) {
-                if (rnd.nextInt(9) >= 4) {
-                    carPositions[i] += 1;
-                }
-            }
-            //System.out.println(i+"번째 자동차 " + carPositions[i]);
-        }
-        print_result(carPositions);
     }
 }
