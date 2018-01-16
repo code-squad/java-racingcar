6857fa0a2c1c6d8a1c46060389988f5609ab11d5
JaeyeonKIM <foodsksms@gmail.com>
2018-01-16 20:54:11 +0900

    Step4 test code setting

---
+++ src/test/java/CarTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CarTest {
+    Car car;
+
+    @Before
+    public void setUp() {
+        
+    }
+}
