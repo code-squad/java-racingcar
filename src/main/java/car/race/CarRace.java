df76e1ee37108f7b0ffbe5cd0df38d5b74d74ce1
Jae Park <jaeepark@gmail.com>
2018-03-05 17:24:36 +0900

    Divided the program into 4 different classes: CarRace, CarRaceMain, Input, Output.

---
+++ src/main/java/car/race/CarRace.java
@@ -0,0 +1,20 @@
+package car.race;
+
+import java.util.Random;
+
+
+public class CarRace {
+	
+	public int generateRandNum() {
+		Random rand = new Random();
+		int randNum = rand.nextInt();
+
+		return randNum;
+	}
+	
+	
+	
+
+	
+	
+}
---
+++ src/main/java/car/race/Input.java
@@ -0,0 +1,22 @@
+package car.race;
+
+import java.util.Scanner;
+
+public class Input {
+	public int askNumberofCars() {
+		Scanner sc = new Scanner(System.in);
+		int cars = sc.nextInt();
+		// sc.close();
+		return cars;
+	}
+	
+	
+	public int askNumberofMoves() {
+		Scanner sc = new Scanner(System.in);
+		int moves = sc.nextInt();
+		sc.close();
+		return moves;
+		
+	}
+	
+}
