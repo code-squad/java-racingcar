0fdd5ea7765cb7e72e188aa75d0595710f34688d
durin93 <lsc401@naver.com>
2018-03-05 20:35:56 +0900

    step3 feedback update

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,110 +1,28 @@
 package racingcar;
 
-import java.util.Random;
-import java.util.Scanner;
-
 public class Car {
-	Scanner sc = new Scanner(System.in);
 
-	String[] inputCar() {
-		System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-		String input = sc.nextLine();
-		String[] names = input.split(",");
-		return names;
+	private String name;
+	private int position = 0;
+
+	public Car(String name) {
+		this.name = name;
 	}
 
-	int input(String names[]) { // 입력받기
-		System.out.println("시도할 횟수는 몇 회 인가요?");
-		int runNum = sc.nextInt();
-
-		return runNum;
+	public String getName() {
+		return name;
 	}
 
-	int[] run(int[] carPositions, int runNum, int carNum) {
-		for (int i = 1; i <= runNum; i++) {
-			chkCarPosition(carPositions, carNum);
-		}
-		return carPositions;
+	public void setName(String name) {
+		this.name = name;
 	}
 
-	int[] chkCarPosition(int[] carPositions, int carNum) {
-		for (int k = 0; k < carNum; k++) {
-			carPositions = check(k, carPositions);
-		}
-		return carPositions;
+	public int getPosition() {
+		return position;
 	}
 
-	int[] check(int k, int[] carPositions) {
-		Random rand = new Random();
-		int randMove = rand.nextInt(10);
-
-		if (randMove >= 4) {
-			carPositions[k] += 1;
-		}
-		return carPositions;
+	public void setPosition(int position) {
+		this.position = position;
 	}
 
-	void resultPrint(int[] carPositions, String[] names) {
-		System.out.println("실행 결과");
-
-		for (int i = 0; i < names.length; i++) {
-
-			System.out.print(names[i]);
-
-			barPrint(i, carPositions);
-			System.out.println();
-
-		}
-
-		lastPrint(carPositions, names);
-
-	}
-
-	void lastPrint(int[] carPositions, String[] names) {
-		int big = 0;
-
-		big = compareResult(names, big, carPositions);
-
-		winnerList(names, big, carPositions);
-
-		System.out.println("가 최종 우승 하였습니다.");
-
-	}
-
-	int compareResult(String[] names, int big, int[] carPositions) {
-		for (int i = 0; i < names.length; i++) {
-
-			if (big < carPositions[i]) {
-				big = carPositions[i];
-			}
-		}
-
-		return big;
-	}
-
-	void winnerList(String[] names, int big, int[] carPositions) {
-		for (int i = 0; i < names.length; i++) {
-			if (big == carPositions[i]) {
-				System.out.print(names[i] + " ");
-			}
-		}
-	}
-
-	void barPrint(int i, int[] carPositions) {
-
-		for (int k = 0; k < carPositions[i]; k++) {
-			System.out.print("-");
-		}
-	}
-
-	public static void main(String[] args) {
-
-		Car car = new Car();
-		String names[] = car.inputCar();
-		int[] carPositions = new int[names.length];
-
-		carPositions = car.run(carPositions, car.input(names), names.length);
-		car.resultPrint(carPositions, names);
-
-	}
 }
