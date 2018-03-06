940236c9504efd2626663c66cad2037af7e850a0
durin93 <lsc401@naver.com>
2018-03-06 18:13:09 +0900

    step4

--- src/test/java/racingcar/test/CarResultTest.java
+++ src/test/java/racingcar/test/CarResultTest.java
@@ -2,16 +2,28 @@ package racingcar.test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 import racingcar.*;
 
 public class CarResultTest {
 
-	Car test1 = new Car("테스트1");
-	Car test2 = new Car("테스트2");
-	Car test3 = new Car("테스트3");
-	Car[] car = { test1, test2, test3 };
+	Car[] car = null;
+	Car test1;
+	Car test2;
+	Car test3;
 
+	
+	@Before
+	public void setUp() {
+		test1 = new Car("test1");
+		test2 = new Car("test2");
+		test3 = new Car("test3");
+	    car = new Car[]{test1, test2, test3}; 
+	}
+	
+
+	
 	@Test
 	public void testMaxCompare() { 
 //		자동차 목록의 위치 값이 가장 큰 값을 찾는 메소드에 대한 테스트 코드를 추가한다.
