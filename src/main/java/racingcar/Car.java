c5f06ee993d5a7b2bda7ebba1a945c33adf814f2
caleb0308 <36419185+caleb0308@users.noreply.github.com>
2018-03-06 11:06:42 +0900

    Step2 (#155)
    * level2
    
    * level2

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -7,19 +7,18 @@ public class Car {
 		Scanner scanner = new Scanner(System.in);
 		Random random = new Random();
 		
-		System.out.println("자동차 대수는 몇 대 인가요?");
-		int cars = carNum(scanner);
+
+		System.out.println("자동차 대수는 몇대인가요?");
+		String[] carName = carArr(scanner);
 		System.out.println("시도할 횟수는 몇 회 인가요?");
 		int tries = tryNum(scanner);
-		run(cars, tries, random);
-		
-		
-		
+		run(carName, tries, random);
 	}
 	
-	static int carNum(Scanner scanner) {   // 자동차 대수 입력메서드
-		int car = scanner.nextInt();
-		return car;
+	static String[] carArr(Scanner scanner) {   // 자동차이름 문자열 배열 메서드
+		int cars = scanner.nextInt();
+		String[] name = new String[cars];
+		return name;
 	}
 	
 	static int tryNum(Scanner scanner) {   // 시도할 횟수 입력메서드
@@ -32,16 +31,21 @@ public class Car {
 		return rnd;
 	}
 	
-	static void run(int cars, int tries, Random random) {   // 주행메서드
-		for (int i = 0; i < cars; i++) {
-			for (int j = 0; j < tries; j++) {
-				int rnd = rndNum(random);
-				if (rnd > 3) {
-					System.out.print("-");
-				}
-			}
-			System.out.println();
+
+	static void run(String[] carArr, int tries, Random random) {   // 주행메서드
+		for (int i = 0; i < carArr.length; i++) {
+			run2(tries, random);
 		}
 			
+	}
+	
+	static void run2(int tries, Random random) {  // 주행메서드 2
+		for (int j = 0; j < tries; j++) {
+			int rnd = rndNum(random);
+			if (rnd > 3) {
+				System.out.print("-");
+			}
+		}
+		System.out.println();
 	}
 }
