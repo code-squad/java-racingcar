259533e7119f1d996334e8472f96e1b7653671a3
imjinbro <jinbro@likelion.org>
2018-03-06 11:44:23 +0900

    step3 피드백 반영 재수정

--- src/main/java/Racing.java
+++ src/main/java/Racing.java
@@ -6,19 +6,7 @@ public class Racing {
     private ArrayList<Car> carList = new ArrayList<>();
     private int times;
 
-    public void setGame(){
-        String playersName = getPlayerInfo();
-        setPlayersCar(playersName);
-
-        int cycleTimes = getCycleTimes();
-        setCycleTimes(cycleTimes);
-    }
-
-    private String getPlayerInfo(){
-        return RacingUtils.getUserInputStr("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-    }
-
-    private void setPlayersCar(String playersName){
+    public void setPlayersCar(String playersName){
         String[] playerNameArr = RacingUtils.parseUsersName(playersName);
 
         for(String name : playerNameArr){
@@ -30,11 +18,7 @@ public class Racing {
         carList.add(new Car(name));
     }
 
-    private int getCycleTimes(){
-        return RacingUtils.getUserInputNumber("시도할 회수는 몇 회 인가요?");
-    }
-
-    private void setCycleTimes(int num){
+    public void setCycleTimes(int num){
         times = num;
     }
 
--- src/main/java/RacingConsole.java
+++ src/main/java/RacingConsole.java
@@ -1,3 +1,6 @@
+import io.Input;
+import io.Output;
+
 public class RacingConsole {
     public static void main(String[] args) {
         RacingConsole.start();
@@ -5,8 +8,38 @@ public class RacingConsole {
 
     static void start(){
         Racing racing = new Racing();
-        racing.setGame();
+        racing.setPlayersCar(getPlayerNames());
+        racing.setCycleTimes(getCycleTimes());
         racing.run();
         racing.showResult();
+    }
+
+    private static String getPlayerNames(){
+        String names = "";
+
+        while(isInvalidNames(names)){
+            Output.printMessage("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+            names = Input.getString();
+        }
+        return names;
+    }
+
+    private static boolean isInvalidNames(String str){
+        return str == null || str.isEmpty();
+    }
+
+    private static int getCycleTimes(){
+        int times = -1;
+
+        while(isInvalidCycleTimes(times)){
+            Output.printMessage("시도할 회수는 몇 회 인가요?");
+            times = Input.getInt();
+        }
+
+        return times;
+    }
+
+    private static boolean isInvalidCycleTimes(int num){
+        return num < 0;
     }
 }
