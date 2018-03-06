07234b647a1d3fbadaa63da1c5e52bf1be32c347
KangGilHwan <36291553+KangGilHwan@users.noreply.github.com>
2018-03-06 10:36:39 +0900

    Car race2 (#153)

--- src/main/java/racing/Car.java
+++ src/main/java/racing/Car.java
@@ -1,23 +1,29 @@
-package racing;
-
 import java.util.Random;
 import java.util.Scanner;
 
 public class Car {
   
-  public void run(int car, int num) {
+  public void run(int car, int num) {//모든 차의 주행 거리를 표현
 	  Random ran = new Random();
 	  
     for(int i = 0; i < car; i++) {
-    	for(int j = 0; j < num; j++) {
-    		int r = ran.nextInt(9);
-    		
-    		if(r >= 4) {
-    			System.out.print("-");// TODO 구현
-    		}
-    	}
-    	System.out.println("");
-    }
+    	onecar(ran, num);
+  }
+ }
+  
+  public void onecar(Random ran, int num) {//한 차의 주행 거리를 표현
+    for(int j = 0; j < num; j++) {
+		rand(ran);
+	}
+	System.out.println("");
+  }
+    
+  public void rand(Random ran) {//랜덤 숫자를 비교해 전진, 정지를 표현
+	  int r = ran.nextInt(9);
+		
+		if(r >= 4) {
+			System.out.print("-");// TODO 구현
+		}
   }
   
   public static void main(String args[]) {
@@ -30,7 +36,7 @@ public class Car {
 	  System.out.println("시도할 횟수는 몇 회 인가요?");
 	  int car = nn.nextInt();
 	  
-	  race.run(num, car);
+	 race.run(num, car);
 	  
   }
 }
