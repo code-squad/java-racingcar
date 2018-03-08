c5d545dadfa056a20f12109501ef66a93a3a1099
JoeChun <godlovesjoe@naver.com>
2018-03-08 20:37:16 +0900

    Step3 (#173)
    * racingCar ver2
    
    * step3 1st ver.
    
    * implements Car
    
    * fixing convention
    
    * class name carMain to CarMain chaged
    
    * after many feedbacks make ver2 of step3

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,37 +1,25 @@
 package racingcar;
 
-import java.util.Random;
-
 public class Car {
 	
-	//random number
-	public int makeRandomNumber() {
-		Random random = new Random();
-		return random.nextInt(10);
+	private int position = 0;
+	private String name;
+	
+	public Car(String name) {
+		this.name = name;
 	}
 	
-	//print dash
-	public void printDash(int rd) {
-		if(rd>=4) {
-			System.out.print("-");
+	public String getName() {
+		return name;
+	}
+	
+	public int getPosition() {
+		return position;
+	}
+	
+	public void go(int randomNumber) {
+		if (randomNumber >= 4) {
+			position++;
 		}
 	}
-	
-	//loop for each cars
-	public void eachCars(int tryNum) {
-		for(int c=0; c< tryNum; c++) {
-			int rd = makeRandomNumber();
-			printDash(rd);
-		}
-		System.out.println("");
-	}
-	
-	//loop as many as user want
-	public void race(int carNum, int tryNum) {
-		for(int i=0; i<carNum; i++) {
-			eachCars(tryNum);
-		}
-	}
-	
-
 }
