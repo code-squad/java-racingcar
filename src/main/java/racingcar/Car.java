c8dc2bea7d9543d7727ffe7939617f00aac2da03
David <xodnjs2549@naver.com>
2018-03-08 15:57:25 +0900

    deleted. go, stop method, add move method in Car class

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -7,13 +7,17 @@ public class Car {
 	public Car(String name) {
 		this.name = name;
 	}
+	
+	public boolean move(int randomNumber) { // go(), stop()을 만드는 것이 아니라 움직였는지 아닌지를 판단하면 move() 하나로 끝낼 수 있다.
+		if( randomNumber >= 4 ) {
+			this.position++;
+			return true;
+		}
+		return false;
+	}
 
 	public int getPosition() {
 		return position;
-	}
-
-	public void setPosition(int position) {
-		this.position = position;
 	}
 
 	public String getName() {
--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -6,13 +6,19 @@ import java.util.Scanner;
 
 public class Racing {
 	// private int time;
-	private String[] registeredCarNames;
-	private Car[] registeredCar;
+	// private String[] carsNames;
+	// main내 지역변수로 자동차 이름을 받고, 자동차 객체를 만들 때 자동차 이름을 받은 배열을 인자로 넘긴다.
+	// 굳이 registered를 붙이지 않아도 충분히 구분 가능하므로 줄이는 게 코드 가독성 높이기에 좋다.
+	private Car[] cars;
 
-	private void makeCar(int carCount) {
-		this.registeredCar = new Car[carCount];
-		for (int i = 0; i < carCount; i++) {
-			this.registeredCar[i] = new Car(this.registeredCarNames[i]);
+	public Racing(String[] carNames) {
+		this.makeCar(carNames);
+	}
+
+	private void makeCar(String[] carNames) {
+		this.cars = new Car[carNames.length]; // 무조건 공간을 먼저 확보해야 하는가?
+		for (int i = 0; i < cars.length; i++) {
+			this.cars[i] = new Car(carNames[i]);
 		}
 	}
 
@@ -23,29 +29,13 @@ public class Racing {
 	}
 
 	private void challenge() {
-		for (int i = 0; i < this.registeredCar.length; i++) {
-			int randomNumber = getRandomNumber();
-			assort(randomNumber, i);
+		for (int i = 0; i < this.cars.length; i++) {
+			this.cars[i].move(getRandomNumber());
 		}
 	}
 
-	private void go(int index) {
-		int position = this.registeredCar[index].getPosition();
-		position++;
-		this.registeredCar[index].setPosition(position);
-	}
-
-	private void stop() {
-		// TODO stop된 상태를 log로 표시하기	
-		}
-
-	// go인지 stop인지 분류
-	private void assort(int randomNumber, int index) {
-		if (randomNumber >= 4) {
-			go(index);
-		}
-		stop();
-	}
+	// go(), stop() 삭제. go, stop은 Car객체의 행동이기 때문에 Car Class로 들어간다.
+	// assort() 삭제. go(), stop()은 move()로 묶을 수 있으며, Ture/False로 move의 여부를 판단할 수 있다.
 
 	private int getRandomNumber() {
 		Random random = new Random();
@@ -55,35 +45,42 @@ public class Racing {
 
 	private ArrayList<String> rank() {
 		ArrayList<String> winners = new ArrayList<String>();
-
 		int winnerScore = 0;
-		for (int i = 0; i < this.registeredCar.length; i++) {
-			int position = this.registeredCar[i].getPosition();
-			String winnerName = this.registeredCar[i].getName();
 
-			if (winnerScore < position) {
-				winnerScore = position;
-				winners.clear(); // ArrayList를 비운다.
-				winners.add(winnerName);
-			} else if (winnerScore == position) {
-				winners.add(winnerName);
-			} else if (winnerScore > position) {
-				continue;
-			}
+		for (int i = 0; i < this.cars.length; i++) {
+			winnerScore = this.compare(winners, winnerScore, i);
 		}
 		return winners;
 	}
 
-	public static void main(String[] args) {
-		Racing race = new Racing();
-		Scanner scanner = new Scanner(System.in);
+	public int compare(ArrayList<String> winners, int winnerScore, int index) {
+		int position = this.cars[index].getPosition();
+		String winnerName = this.cars[index].getName();
 
-		race.registeredCarNames = InputView.inputCarNames(scanner);
+		if (winnerScore < position) {
+			winnerScore = position;
+			winners.clear(); // ArrayList를 비운다.
+			winners.add(winnerName);
+		} else if (winnerScore != 0 && winnerScore == position) {
+			winners.add(winnerName);
+		}
+
+		return winnerScore;
+	}
+
+	public static void setupRace() {
+		Scanner scanner = new Scanner(System.in);
+		String[] carNames = InputView.inputCarNames(scanner);
 		int challengeCount = InputView.inputChallengeCount(scanner);
-		int carCount = race.registeredCarNames.length;
-		race.makeCar(carCount); // 자동차 생성(이름 등록)
+
+		Racing race = new Racing(carNames); // new 하는 순간 heap에 Racing 객체가 만들어지고, 해당 인스턴스의 멤버변수가 생성된다.
 		race.start(challengeCount);
-		ResultView.output(race.registeredCar, race.rank());
+		ResultView.output(race.cars, race.rank());
+
 		scanner.close();
+	}
+
+	public static void main(String[] args) {
+		setupRace();
 	}
 }
