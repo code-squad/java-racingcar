1ec0764d2ba2e076ae1769baa0ca7fc80ff7832d
durin93 <lsc401@naver.com>
2018-03-08 10:06:09 +0900

    step4 feedback update

--- src/test/java/racingcar/test/CarResultTest.java
+++ src/test/java/racingcar/test/CarResultTest.java
@@ -13,40 +13,68 @@ public class CarResultTest {
 	Car test2;
 	Car test3;
 
-	
 	@Before
 	public void setUp() {
 		test1 = new Car("test1");
 		test2 = new Car("test2");
 		test3 = new Car("test3");
-	    car = new Car[]{test1, test2, test3}; 
+		car = new Car[] { test1, test2, test3 };
 	}
-	
 
-	
 	@Test
-	public void testMaxCompare() { 
-//		자동차 목록의 위치 값이 가장 큰 값을 찾는 메소드에 대한 테스트 코드를 추가한다.
+	public void testMaxCompare() {
+		// test1의 포지션이 2인경우를 가정
 		test1.updatePosition();
+		test1.updatePosition();
+
 		int max = 0;
 		for (int i = 0; i < car.length; i++) {
 			if (max < car[i].getPosition()) {
 				max = car[i].getPosition();
+				assertEquals(2, car[0].getPosition());
 			}
 		}
-		assertEquals(max, test1.getPosition());
+	}
+
+	// 자동차 목록의 위치 값이 가장 큰 값을 찾는 메소드에 대한 테스트 코드를 추가한다.
+	@Test
+	public void testMaxCompareResult() {
+		int max = 0;
+		// test1의 포지션이 2인경우를 가정
+		test1.updatePosition();
+		test1.updatePosition();
+
+		if (max < car[0].getPosition()) {
+			max = car[0].getPosition();
+		}
+		assertEquals(2, max);
 	}
 
 	@Test
-	public void textMax() {
-//		자동차 목록에서 가장 큰 위치 값에 해당하는 자동차 목록을 구하는 메소드에 대한 테스트 코드를 추가한다.
+	public void testWinnerCheck() {
+		// 자동차 목록에서 가장 큰 위치 값에 해당하는 자동차 목록을 구하는 메소드에 대한 테스트 코드를 추가한다.
 		int max = 2;
+		test1.updatePosition();
+		test1.updatePosition();
 		for (int i = 0; i < car.length; i++) {
 			if (max == car[i].getPosition()) {
-				assertEquals(max, car[i].getPosition());
+				assertEquals(max, car[0].getPosition());
 			}
 		}
-
 	}
+
+	@Test
+	public void testWinnerResult() {
+		int max = 2;
+		test1.updatePosition();
+		test1.updatePosition();
+
+		if (max == car[0].getPosition()) {
+			assertEquals("test1", car[0].getName());
+		}
+	}
+	
+	
+	
 
 }
--- src/test/java/racingcar/test/CarTest.java
+++ src/test/java/racingcar/test/CarTest.java
@@ -18,29 +18,23 @@ public class CarTest { // 자동차를 이동(move 메소드)하는 메소드에
 	}
 
 	@Test
-	public void moveUp() {
+	public void testUpdatePosition() {
 		// 4이상의 값이 경우 위치 값이 1 증가했는가?
-
-		int basePosition = car.getPosition();
-
 		int rNum = 5;
 		if (rNum > 4) {
 			car.updatePosition();
 		}
-		assertEquals((basePosition + 1), car.getPosition());
+		assertEquals(1, car.getPosition());
 	}
 
 	@Test
-	public void moveDown() {
+	public void testUpdatePositionOtherwise() {
 		// 4미만의 값일 경우 위치 값이 증가하지 않았는가?
-
-		int basePosition = car.getPosition();
-
 		int rNum = 3;
 		if (rNum > 4) {
 			car.updatePosition();
 		}
-		assertEquals(basePosition, car.getPosition());
+		assertEquals(0, car.getPosition());
 	}
 
 }
