57b7e18b13fe30712a999e8f3eb6fb7bdd30213e
larryjung <corbswjd@gmail.com>
2018-03-08 16:03:07 +0900

    code convention consideration

--- src/main/java/CarOutput.java
+++ src/main/java/CarOutput.java
@@ -1,26 +1,26 @@
 public class CarOutput {
 
-    private static void runningExpression(Car c) {
-        System.out.printf("Car " + c.getName() + " >> ");
-        for (int i = 0; i < c.getPosition(); i++) {
+    private static void runningExpression(Car car) {
+        System.out.printf("Car " + car.getName() + " >> ");
+        for (int i = 0; i < car.getPosition(); i++) {
             System.out.printf("-");
         }
         System.out.println("");
     }
 
-    public static void printRacing(Racing r) {
-        for (Car c : r.getCars()) {
+    public static void printRacing(Racing racing) {
+        for (Car c : racing.getCars()) {
             runningExpression(c);
         }
     }
 
-    public static void printWinner(Racing r) {
-        System.out.println("우승자는 " + r.getWinnerList() + "입니다.");
+    public static void printWinner(Racing racing) {
+        System.out.println("우승자는 " + racing.getWinnerList() + "입니다.");
     }
 
-    public static void printResult(Racing r) {
-        printRacing(r);
-        printWinner(r);
+    public static void printResult(Racing racing) {
+        printRacing(racing);
+        printWinner(racing);
     }
 }
 
