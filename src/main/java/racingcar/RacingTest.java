15e4ea35392e7e18c303a12008341e245ca62143
David <xodnjs2549@naver.com>
2018-03-08 20:22:56 +0900

    add @test compare method

--- src/main/java/racingcar/RacingTest.java
+++ src/main/java/racingcar/RacingTest.java
@@ -12,7 +12,7 @@ public class RacingTest {
 	String[] carNames = { "pobi", "honux", "crong", "JK" };
 	Car[] cars;
 	ArrayList<String> winners;
-
+	
 	@Before
 	public void setUp() {
 		racing = new Racing(carNames);
@@ -28,24 +28,25 @@ public class RacingTest {
 		cars[2].move(4);
 		cars[3].move(4);
 		cars[3].move(4);
+		
+		winners = racing.rank();
 	}
 
 	// 공동 우승자는 pobi, honux. 즉 winners size도 2
 	@Test
 	public void rank() {
-		winners = racing.rank();
 		assertEquals("crong", winners.get(0));
 		assertEquals("JK", winners.get(1));
 		assertEquals(2, winners.size());
 	}
 
-//	@Test
-//	public void compare() {
-//		int winnerScore = 0;
-//		
-//		for (int i = 0; i < cars.length; i++) {
-//			winnerScore = racing.compare(winners, winnerScore, i);
-//		}
-//		assertEquals(2, winnerScore);
-//	}
+	@Test
+	public void compare() {
+		int winnerScore = 0;
+		for (int i = 0; i < cars.length; i++) {
+			winnerScore = racing.compare(winners, winnerScore, i);
+		}
+		
+		assertEquals(2, winnerScore);
+	}
 }
