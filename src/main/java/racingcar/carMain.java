79463fb6cdd4aba26f22f64046f59ca54e5acf02
HYEON SEONG CHUN <godlovesjoe@naver.com>
2018-03-08 11:52:56 +0900

    fixing convention

--- src/main/java/racingcar/carMain.java
+++ src/main/java/racingcar/carMain.java
@@ -3,12 +3,11 @@ package racingcar;
 import java.util.Scanner;
 
 public class carMain {
-<<<<<<< HEAD
 	
 	//make Car Array
 	public Car[] carArray(String[] name) {
 		Car car[] = new Car[name.length];
-		for(int i=0; i<name.length; i++) {
+		for (int i = 0; i < name.length; i++) {
 			car[i]=new Car(name[i]);
 		}
 		return car;
@@ -16,7 +15,7 @@ public class carMain {
 	
 	//each car Status
 	public static void eachCars(Car[] car) {
-		for(int c=0; c< car.length; c++) {
+		for (int c = 0; c < car.length; c++) {
 			Car cars = car[c];
 			int rd = cars.makeRandomNumber();
 			cars.go(rd);
@@ -36,7 +35,7 @@ public class carMain {
 	
 	//repeat the games
 	public static void repeatGame(int tryNum, Car[] car) {
-		for(int i=0; i<tryNum; i++) {
+		for (int i = 0; i < tryNum; i++) {
 			eachCars(car);
 			System.out.println("");
 		}
@@ -45,8 +44,8 @@ public class carMain {
 	//winner
 	public static int winner(Car[] car) {
 		int winner = 0;
-		for(int i=0; i<car.length; i++) {
-			if(winner <= car[i].getPosition()) {
+		for (int i = 0; i < car.length; i++) {
+			if (winner <= car[i].getPosition()) {
 				winner = car[i].getPosition();
 			}
 		}
@@ -67,19 +66,5 @@ public class carMain {
 		
 		int Winner = winner(car);
 		ResultView.getWinner(car, Winner);
-=======
-
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		Car racing = new Car();
-		System.out.println("몇 대 있나요?");
-		int carNum = scan.nextInt();
-		
-		System.out.println("몇 번 하실 건가요?");
-		int tryNum = scan.nextInt();
-		
-		racing.race(carNum, tryNum);
->>>>>>> JosephChun
 	}
-
 }
