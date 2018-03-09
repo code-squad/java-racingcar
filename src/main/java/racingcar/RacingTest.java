c6fa6e6003551e51542d967b0d62145ab1afcf33
David <xodnjs2549@naver.com>
2018-03-09 15:36:42 +0900

    edited. RacingTest Comment

--- src/main/java/racingcar/RacingTest.java
+++ src/main/java/racingcar/RacingTest.java
@@ -36,7 +36,9 @@ public class RacingTest {
 	 *  problem
 	 *  compare() 테스트에서 winners에 NullPointerException 에러가 난다.
 	 *  우승자 목록인 ArrayList<String> winners의 초기화를 rank()함수의 리턴값으로 하기 때문에.
-	 *  JUit Test 특성상 테스트 메서드는 서로 독립적이어야 하고, 어떤 메서드가 먼저 호출될 지 모른다.
+	 *  원래 소스에서는 내가 정한 흐름대로 실행.
+	 *  그러나 JUit Test 특성상 테스트 메서드는, 어떤 메서드가 먼저 호출될 지 모른다.
+	 *  따라서 작성되는 모든 테스트 메서드는 다른 메서드에 종속되어 있으면 테스트에 실패할 수 있다.
 	 *  solution
 	 *  RacingTest의 멤버 변수로 빼고, 변수 선언과 동시에 new를 통하여 초기화만 진행한다.
 	 *  이전에는 별도의 초기화 없이, 완성된 winners를 가지고 초기화없이 값 세팅을 하려고 했다.  
