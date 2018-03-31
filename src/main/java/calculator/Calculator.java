a4285081ec6f1d103dfbadbb7357e96f2dc80cdd
sangsik.kim <sskim@rodaloft.com>
2018-03-31 16:38:29 +0900

    메소드명 변경

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -6,20 +6,20 @@ package calculator;
 public class Calculator {
 
 
-    public static int calculate(String text) {
+    public static int calculateTwoNumbers(String text) {
         String[] values = text.split(" ");
 
         int result = 0;
         for (int i = 0; i < values.length - 1; i++) {
             if (values[i].equals("+") || values[i].equals("-") || values[i].equals("*") || values[i].equals("/")) {
-                result = calculation(Integer.parseInt(values[i - 1]), Integer.parseInt(values[i + 1]), values[i]);
+                result = calculateTwoNumbers(Integer.parseInt(values[i - 1]), Integer.parseInt(values[i + 1]), values[i]);
                 values[i + 1] = String.valueOf(result);
             }
         }
         return result;
     }
 
-    private static int calculation(int firstNumber, int secondNumber, String operator) {
+    private static int calculateTwoNumbers(int firstNumber, int secondNumber, String operator) {
         if (operator.equals("+")) {
             return firstNumber + secondNumber;
         }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -11,37 +11,37 @@ public class CalculatorTest {
 
     @Test
     public void sum() {
-        int result = Calculator.calculate("1 + 2");
+        int result = Calculator.calculateTwoNumbers("1 + 2");
         assertThat(result).isEqualTo(3);
     }
 
     @Test
     public void subtract() {
-        int result = Calculator.calculate("5 - 3");
+        int result = Calculator.calculateTwoNumbers("5 - 3");
         assertThat(result).isEqualTo(2);
     }
 
     @Test
     public void multiply() {
-        int result = Calculator.calculate("5 * 2");
+        int result = Calculator.calculateTwoNumbers("5 * 2");
         assertThat(result).isEqualTo(10);
     }
 
     @Test
     public void divide() {
-        int result = Calculator.calculate("6 / 2");
+        int result = Calculator.calculateTwoNumbers("6 / 2");
         assertThat(result).isEqualTo(3);
     }
 
     @Test
     public void 입력값이_3개() {
-        int result = Calculator.calculate("1 + 2 + 3");
+        int result = Calculator.calculateTwoNumbers("1 + 2 + 3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
     public void 복합계산() {
-        int result = Calculator.calculate("1 + 2 - 3 + 10 / 2");
+        int result = Calculator.calculateTwoNumbers("1 + 2 - 3 + 10 / 2");
         assertThat(result).isEqualTo(5);
     }
 
