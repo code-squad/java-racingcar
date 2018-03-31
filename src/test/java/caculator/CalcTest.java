982c1aa2416ac47efd8c6e5c9953e0365fb89209
gold <pnurep@gmail.com>
2018-03-31 19:05:56 +0900

    test modified

--- src/test/java/caculator/CalcTest.java
+++ src/test/java/caculator/CalcTest.java
@@ -6,9 +6,11 @@ import static org.assertj.core.api.Assertions.*;
 public class CalcTest {
 
     @Test
-    public void name() {
-        int result = Calc.input("2 + 3 * 4 / 2");
-        assertThat(result).isEqualTo(10);
+    public void calc() {
+        assertThat(Calc.input("2 + 3 * 4 / 2")).isEqualTo(10);
+        assertThat(Calc.input("2 + 8 * 4 / 2")).isEqualTo(20);
+        assertThat(Calc.input("2 + 8 * 4 / 4")).isEqualTo(10);
+        assertThat(Calc.input("5 + 10 * 4 / 2")).isEqualTo(30);
     }
 
     @Test
