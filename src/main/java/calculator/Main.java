a46d71b6ae9a19017e218a90506e4bbfbddf6c1e
shinhong <psh01111@codelinkcorp.com>
2018-03-31 16:59:16 +0900

    add Main class

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 
 public class Main {
 
+    Calculator calculator = new Calculator();
+
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         String value = scanner.nextLine();
@@ -22,14 +24,36 @@ public class Main {
         return (numsSize - operatorLength) == 1;
     }
 
-    public void operate(List<Integer> numbers, String operators){
+    public void mainStage(List<Integer> numbers, String operators){
 
         if(!isValidated(numbers.size(),operators.length())){
-
-
-
+            throw new IllegalArgumentException();
         }
 
+        int result = numbers.get(0);
+
+        for( int i = 1; i < operators.length(); i++){
+            result = operate(result, numbers.get(i), operators.charAt(i-1));
+        }
+
+    }
+
+    private int operate(int x, int y, char operator){
+        if(operator == '+'){
+            return calculator.add(x,y);
+        }
+
+        if(operator == '-'){
+            return calculator.substract(x,y);
+        }
+
+        if(operator == '*'){
+            return calculator.muliply(x,y);
+        }
+
+        if(operator == '/'){
+            return calculator.divide(x,y);
+        }
     }
 
 }
