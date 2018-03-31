4f2f30105e053e84188171a8dae3bbf25fc45962
Jinho Yim <jinhoyim@outlook.com>
2018-03-31 16:43:39 +0900

    first push test (#202)

---
+++ src/main/java/com/calculator/StringCalculatorApp.java
@@ -0,0 +1,11 @@
+package com.calculator;
+
+import java.util.Scanner;
+
+public class StringCalculatorApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String value = scanner.nextLine();
+        System.out.println(value);
+    }
+}
