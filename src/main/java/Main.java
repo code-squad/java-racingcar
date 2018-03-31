4c19ffb72f9521932a19bbdf6340d628175d46c5
Jeff Chung <drifterz303@gmail.com>
2018-03-31 16:35:16 +0900

    first update

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String value = scanner.nextLine();
+
+        String[] tokens = value.split(" ");
+        ArrayList<Integer> numbers =  new ArrayList<Integer>();
+
+//        for (String token : tokens) {
+//
+//
+//        }
+
+
+    }
+
+
+
+}
