b35051835777ddb568a7524631dfccf13826f143
graytraces <graytraces@gmail.com>
2018-03-31 15:45:45 +0900

    Step0 (#185)
    * Make working calculator proto version.
    
    Work well. But need refactoring.
    
    * Add complex test case.
    
    6 param testcase is added

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,46 @@
+package calculator;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CalculatorTest{
+
+    @Test
+    public void runNormalCase() {
+        Calculator calculator = new Calculator();
+        assertThat(calculator.calculate("2 + 3 * 4 / 2")).isEqualTo(10.0);
+    }
+
+    @Test
+    public void run2ParamPlus() {
+        Calculator calculator = new Calculator();
+        assertThat(calculator.calculate("2 + 3")).isEqualTo(5.0);
+    }
+
+    @Test
+    public void run2ParamMinus() {
+        Calculator calculator = new Calculator();
+        assertThat(calculator.calculate("2 - 3")).isEqualTo(-1.0);
+    }
+
+
+    @Test
+    public void run2ParamMulti() {
+        Calculator calculator = new Calculator();
+        assertThat(calculator.calculate("2 * 3")).isEqualTo(2*3);
+    }
+
+
+    @Test
+    public void run2ParamDivide() {
+        Calculator calculator = new Calculator();
+        assertThat(calculator.calculate("2 / 3")).isEqualTo(2d/3d);
+    }
+
+    @Test
+    public void runComplexCase() {
+        Calculator calculator = new Calculator();
+        assertThat(calculator.calculate("3 + 5 * 7 / 4 + 16 / 3")).isEqualTo(10d);
+    }
+}
