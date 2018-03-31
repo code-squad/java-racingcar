433d3b8dfddc9775c09191f10fcb60e128ee0878
ssosso <ssossohow@gmail.com>
2018-03-31 17:10:08 +0900

    calculator 테스트 방식 변경

--- src/test/java/calculater/CalculaterTest.java
+++ src/test/java/calculater/CalculaterTest.java
@@ -1,28 +1,31 @@
 package calculater;
 
+import org.junit.Assert;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 
 public class CalculaterTest {
-	private static final double DELTA = 1e-15;
-	
 	@Test
 	public void 덧셈() {
-		assertEquals(5,Calculater.calculate("2 + 3"), DELTA);
+		Assert.assertEquals(0, Double.compare(5, Calculater.calculate("2 + 3")));
 	}
 	
 	@Test
 	public void 뺄셈() {
-		assertEquals(2, Calculater.calculate("5 - 3"), DELTA);
+		Assert.assertEquals(0, Double.compare(2, Calculater.calculate("5 - 3")));
 	}
 	
 	@Test
 	public void 곱하기() {
-		assertEquals(8, Calculater.calculate("2 * 4"), DELTA);
+		Assert.assertEquals(0, Double.compare(8, Calculater.calculate("2 * 4")));
 	}
 	
 	@Test
 	public void 나누기() {
-		assertEquals(2, Calculater.calculate("10 / 5"), DELTA);
+		Assert.assertEquals(0, Double.compare(2, Calculater.calculate("10 / 5")));
+	}
+	
+	@Test
+	public void 계산() {
+		Assert.assertEquals(0, Double.compare(10, Calculater.calculate("2 + 3 * 4 / 2")));
 	}
 }
