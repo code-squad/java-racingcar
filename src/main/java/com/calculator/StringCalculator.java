6893ec61fd5063e31f0d58ab8e75f281a8477f51
Jinho Yim <jinhoyim@outlook.com>
2018-04-01 12:19:13 +0900

    사칙연산 구현

--- src/main/java/com/calculator/StringCalculator.java
+++ src/main/java/com/calculator/StringCalculator.java
@@ -7,16 +7,91 @@ public class StringCalculator {
 
     }
 
-    public int calculate(String inputExpr) {
-        if(StringUtils.isBlank(inputExpr)) {
+    public int calculate(final String inputExpression) {
+        if (StringUtils.isBlank(inputExpression)) {
             throw new IllegalArgumentException("입력값이 null 또는 공백 문자열입니다.");
         }
 
-        String[] expressions = inputExpr.split(" ");
-        if(expressions.length < 3) {
-            throw new IllegalArgumentException("2개 이상의 피연산자가 필요합니다.");
+        final String[] expressions = splitInputExpr(inputExpression);
+
+        int leftOperand = parseOperand(expressions[0]);
+
+        for (int i = 1; i < expressions.length; i += 2) {
+            final String operator = expressions[i];
+            final int rightOperand = parseOperand(expressions[i + 1]);
+            leftOperand = calculate(operator, leftOperand, rightOperand);
         }
-        return 0;
+
+        return leftOperand;
     }
 
+    private int calculate(final String operator, final int leftOperand, final int rightOperand) {
+        if ("+".equals(operator)) {
+            return add(leftOperand, rightOperand);
+        }
+
+        if ("-".equals(operator)) {
+            return subtract(leftOperand, rightOperand);
+        }
+
+        if ("*".equals(operator)) {
+            return multiply(leftOperand, rightOperand);
+        }
+
+        if ("/".equals(operator)) {
+            if (rightOperand == 0) {
+                throw new IllegalArgumentException("0으로 나눌 수 없습니다.");
+            }
+
+            return divide(leftOperand, rightOperand);
+        }
+
+        throw new IllegalArgumentException("잘못된 연산자입니다.");
+    }
+
+    private int add(final int first, final int second) {
+        return first + second;
+    }
+
+    private int subtract(final int first, final int second) {
+        return first - second;
+    }
+
+    private int multiply(final int first, final int second) {
+        return first * second;
+    }
+
+    private int divide(final int first, final int second) {
+        return first / second;
+    }
+
+    private String[] splitInputExpr(final String inputExpr) {
+        final String[] expressions = inputExpr.split(" ");
+        if (isExpressionsEven(expressions)) {
+            throw new IllegalArgumentException("유효하지 않은 식입니다.");
+        }
+
+        if (isExpressionsLessThanThree(expressions)) {
+            throw new IllegalArgumentException("최소 3개의 식이 필요합니다.");
+        }
+
+        return expressions;
+    }
+
+    private boolean isExpressionsEven(final String[] expressions) {
+        return expressions.length % 2 == 0;
+    }
+
+    private boolean isExpressionsLessThanThree(final String[] expressions) {
+        return expressions.length < 3;
+    }
+
+    private int parseOperand(final String operand) {
+        try {
+            return Integer.parseInt(operand);
+        } catch (final NumberFormatException e) {
+            throw new IllegalArgumentException(
+                    "피연산자 \"" + operand + "\"" + "는 숫자로 변환될 수 없습니다.", e);
+        }
+    }
 }
--- src/test/java/com/calculator/StringCalculatorTest.java
+++ src/test/java/com/calculator/StringCalculatorTest.java
@@ -3,7 +3,7 @@ package com.calculator;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringCalculatorTest {
     private StringCalculator calculator;
@@ -14,22 +14,84 @@ public class StringCalculatorTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void 값이_null일때() {
+    public void 값이_null() {
         calculator.calculate(null);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void 값이_빈문자열일때() {
+    public void 값이_빈문자열() {
         calculator.calculate("");
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void 값이_공백문자열일때() {
+    public void 값이_공백문자열() {
         calculator.calculate(" ");
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void 피연산자가_두개_미만일때() {
-        calculator.calculate("1 + ");
+    public void 분해한_식이_짝수() {
+        calculator.calculate("1 + 2 + ");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void 분해한_식이_하나() {
+        calculator.calculate("1");
+    }
+
+    @Test
+    public void 덧셈() {
+        final int result = calculator.calculate("1 + 2");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    public void 뺄셈() {
+        final int result = calculator.calculate("2 - 1");
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    public void 곱셈() {
+        final int result = calculator.calculate("2 * 3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void 나눗셈() {
+        final int result = calculator.calculate("4 / 2");
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void 피연산자가_숫자가_아닐때() {
+        calculator.calculate("a + 1");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void 사칙연산_이외의_연산자() {
+        calculator.calculate("4 % 5");
+    }
+
+    @Test
+    public void 두번_연산() {
+        final int result = calculator.calculate("1 + 2 + 3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void 세번_연산() {
+        final int result = calculator.calculate("1 + 2 + 3 + 4");
+        assertThat(result).isEqualTo(10);
+    }
+
+    @Test
+    public void 순서대로_연산() {
+        final int result = calculator.calculate("1 + 2 * 3 - 1 / 2");
+        assertThat(result).isEqualTo(4);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void 숫자0으로_나누기() {
+        calculator.calculate("4 / 0");
     }
 }
