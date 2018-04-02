c25789e88fcc6af21abbebf085a2b93100a02c99
Jeff Chung <drifterz303@gmail.com>
2018-04-02 22:08:46 +0900

    문자열 계산기에 for 문을 추가하고 실질적으로 계산하는 함수를 분리한다

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,17 +4,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        String value = scanner.nextLine();
-
-        String[] tokens = value.split(" ");
-        ArrayList<Integer> numbers =  new ArrayList<Integer>();
-
-//        for (String token : tokens) {
-//
-//
-//        }
-
+        int result = Calculator.run("5 + 1 / 2");
+        System.out.println(result);
 
     }
 
