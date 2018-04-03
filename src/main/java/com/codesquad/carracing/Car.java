c4dc5e0f74ebd1ee5e800d3e9654baaa3bf2758b
Jinho Yim <jinhoyim@outlook.com>
2018-04-03 20:24:36 +0900

    기존 move 방식에 대한 주석 제거

--- src/main/java/com/codesquad/carracing/Car.java
+++ src/main/java/com/codesquad/carracing/Car.java
@@ -15,16 +15,6 @@ public class Car {
         return this.position;
     }
 
-//    public void move(final RandomGenerator randomGenerator) {
-//        if (willMove(randomGenerator.generate())) {
-//            this.position++;
-//        }
-//    }
-
-//    private boolean willMove(final int value) {
-//        return value >= 4 && value < 10;
-//    }
-
     public void move(final MoveStrategy moveStrategy) {
         if (moveStrategy.isMovable()) {
             this.position++;
