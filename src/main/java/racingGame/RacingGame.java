efe8749cf521918a11da34d78448cb86cf9a51ad
홍종완 <serverwizard89@gmail.com>
2018-04-05 00:07:59 +0900

    step2 racing game

--- src/main/java/racingGame/RacingGame.java
+++ src/main/java/racingGame/RacingGame.java
@@ -1,9 +1,9 @@
 package racingGame;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.ArrayList;
 import java.util.List;
-
-import static com.sun.tools.internal.xjc.reader.Ring.add;
 
 /**
  * Created by hongjong-wan on 2018. 3. 31..
@@ -12,48 +12,77 @@ public class RacingGame {
 
 
     private static final int MOVE_CONDITION = 4;
-    private List<Car> cars;
+    private List<Car> cars = new ArrayList<>();
+    private List<Car> winnerCars = new ArrayList<>();
 
-
-    public RacingGame(int carNum) {
-        if (carNum < 0) {
+    public RacingGame(String carNames) {
+        if (StringUtils.isBlank(carNames)) {
             throw new IllegalArgumentException();
         }
-        cars = new ArrayList<>();
-        initCarPosition(carNum);
+        addCar(carNames);
     }
 
-    private void initCarPosition(int carNum) {
-        for (int i = 0; i < carNum; i++) {
-            cars.add(new Car(0));
+    private void addCar(String carNames) {
+        String[] carName = carNames.split(",");
+        for (int i = 0; i < carName.length; i++) {
+            cars.add(new Car(carName[i]));
         }
     }
 
 
     public void moveCars(int tryNum) {
-
         if (tryNum < 0) {
             throw new IllegalArgumentException();
         }
 
         for (int i = 0; i < tryNum; i++) {
             makeCarMove();
-            printResult();
+            // 하나의 메소드는 하나의 일만해야 한다고 배웠는데
+            // moveCars()는 아래의 printResult() 메소드 때문에 2가지 역할(이동, 출력)을 하고 있는데
+            // printResult() 메소드를 이렇게 두는것이 맞는 걸까요?
+            // 시도 회수마다 이동 흔적을 출력해야해서 이렇게 구현하긴했는데 찝찝합니다
+            printTrace();
         }
-
     }
 
     private void makeCarMove() {
-
         for (int i = 0; i < cars.size(); i++) {
-            if (MOVE_CONDITION <= RandomGenerator.getRandomNum()) {
-                cars.set(i, cars.get(i).move());
-            }
+            moveCarPosition(i);
         }
     }
 
-    public void printResult() {
-        ResultView.printResult(cars);
+    private void moveCarPosition(int i) {
+        if (MOVE_CONDITION <= RandomGenerator.getRandomNum()) {
+            cars.set(i, cars.get(i).move());
+        }
     }
+
+    public List<Car> findWinnerCars() {
+        for (Car car : cars) {
+            int farthestPosition = findFarthestPosition();
+            int carPosition = car.getPosition();
+
+            if (farthestPosition == carPosition) {
+                winnerCars.add(car);
+            }
+        }
+        return winnerCars;
+    }
+
+    public int findFarthestPosition() {
+        int farthestPosition = 0;
+        for (Car car : cars) {
+            int carPosition = car.getPosition();
+            if (farthestPosition < carPosition) {
+                farthestPosition = carPosition;
+            }
+        }
+        return farthestPosition;
+    }
+
+    public void printTrace() {
+        ResultView.printTrace(cars);
+    }
+
 
 }
--- src/test/java/racingGame/RacingGameTest.java
+++ src/test/java/racingGame/RacingGameTest.java
@@ -1,74 +1,30 @@
 package racingGame;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 /**
  * Created by hongjong-wan on 2018. 3. 31..
  */
 public class RacingGameTest {
 
-    /**
+    private RacingGame racingGame;
 
-     [1]
-        기능구현전 기능 요구사항을 보고 테스트케이스가 3개쯤 될것으로 판단
-
-        # 테스트 케이스 정리
-         1. 자동차 수 (양수,음수)
-         2. 시도 횟수 (양수 음수)
-         3. 이동여부
-
-        하지만 자동차수와 시도횟수에 해당하는 테스트코드를 작성하는 부분에서 의문점이 생김
-
-        왜냐하면 테스트 코드를 작성하려면 사용자가 값을 입력했다고 가정해야하는데
-        그것을 표현하기위해 아래처럼 carNum 변수값을 하드코딩으로 양수로 했다가 다음에는 음수로 했다가 함
-        이런식으로해서 테스트코드를 돌려봄
-
-        테스트코드가 통과되면 그때 하드코딩한 부분을 Scanner로 변경함
-        이런식으로 하는게 맞는것인가 의문이 들었음
-
-        public static int getCarNum() {
-            System.out.print("자동차 수를 입력하세요: ");
-            int carNum = 2;
-
-            if (carNum < 0) {
-                throw new IllegalArgumentException();
-            }
-
-            return carNum;
-        }
-
-
-     [2]
-        production코드에서 racing() 메소드가 핵심이라고 생각하는데
-        해당 부분을 테스트코드로 작성하는것은 더더욱 어려웠음
-
-        racing() 메소드 안에 있는 getRandom() 메소드야 테스트 코드 수행을위해
-        특정 숫자로 변경한다하더라도 display() 메소드부분은 어떻게해야하는지 모르겠음
-
+    /*
+        테스트코드 짜기가 너무 어렵습니다
+        제가 구현한 코드가 테스트하기 어려운걸까요?
+        아니면 아직 익숙하지 않아서일까요?
+        그리고 테스트하기 좋게 코드짜는 법이 있나요? ㅠ
      */
 
-//    @Test
-//    public void 자동차수_양수() {
-//        assertEquals(2, RacingGame.getCarNum());
-//    }
-//
-//    @Test(expected = IllegalArgumentException.class)
-//    public void 자동차수_음수() {
-//        assertEquals(-2, RacingGame.getCarNum());
-//    }
+    @Before
+    public void setUp() {
+        racingGame = new RacingGame("aaa,bbb,ccc");
+    }
 
 
-
-//    @Test
-//    public void 시도횟수_양수() {
-//        assertEquals(2, RacingGame.getTryCount());
-//    }
-//
-//    @Test(expected = IllegalArgumentException.class)
-//    public void 시도횟수_음수() {
-//        assertEquals(-2, RacingGame.getTryCount());
-//    }
 
 }
