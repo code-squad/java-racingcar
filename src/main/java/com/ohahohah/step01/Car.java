ff6e2798849dd66081066a55ba94bd56b11a95ce
SiYoungOh(ohahohah) <ohahohah.dev@gmail.com>
2018-04-04 22:46:58 +0900

    - random생성값과 로직처리 부분 분리 - 자동차 현재 위치 문자열로 표현부분 - 문자열 생성과 문자열 출력으로 분리

--- src/main/java/com/ohahohah/step01/Car.java
+++ src/main/java/com/ohahohah/step01/Car.java
@@ -1,38 +1,38 @@
 package com.ohahohah.step01;
 
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Stream;
 
 public class Car {
 
   private int playerNum;
-  private int progress;
+  private int coordinate;
 
-  public Car(int playerNum, int progress) {
+  public Car(int playerNum, int coordinate) {
     this.playerNum = playerNum;
-    this.progress = progress;
+    this.coordinate = coordinate;
   }
 
-  public void stopOrGo() {
-    if (isForwarding()) {
-      moveForward();
+  public int getCoordinate() {
+    return coordinate;
+  }
+
+  public int stopOrGo(int randomValue) {
+    if(randomValue >= 4){
+      return 1;
     }
+    return 0;
   }
 
-  public void moveForward() {
-    progress += 1;
+  public void moveForward(int i) {
+    coordinate +=i;
   }
 
-  public boolean isForwarding() {
-    if (ThreadLocalRandom.current().nextInt(0, 9 + 1) >= 4) {
-      return true;
+  //TODO go to view
+  public String coordinateToStr() {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < coordinate; i++) {
+      sb.append("-");
     }
-    return false;
-  }
-
-  public void printRaceStatus() {
-    for (int i = 0; i < progress; i++) {
-      System.out.print("-");
-    }
-    System.out.println();
+    return sb.toString();
   }
 }
--- src/test/java/com/ohahohah/step01/CarTest.java
+++ src/test/java/com/ohahohah/step01/CarTest.java
@@ -1,30 +1,42 @@
 package com.ohahohah.step01;
 
-import static org.junit.Assert.*;
-
-import java.util.concurrent.ThreadLocalRandom;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
+
   Car car;
 
   @Before
   public void setUp() throws Exception {
-    car = new Car(1,0);
+    car = new Car(1, 0);
   }
 
   @After
   public void tearDown() throws Exception {
   }
 
-  //즉시 출력할 경우, 이동횟수가 필요없어서 삭제함
-//  @Test
-//  public void 전진_또는_멈출_경우_이동횟수가_증가하는지_확인() {
-//    int currentTurn = car.getTurn();
-//    car.stopOrGo();
-//    assertThat(car.getTurn()).isEqualTo(currentTurn+1);
-//  }
+  @Test
+  public void 숫자에_따라서_전진횟수가_출력되는지_확인() {
+    assertThat(car.stopOrGo(0)).isEqualTo(0);
+    assertThat(car.stopOrGo(4)).isEqualTo(1);
+    assertThat(car.stopOrGo(9)).isEqualTo(1);
+  }
+
+  @Test
+  public void 입력값에_따라_자동차_위치가_변화하는지_확인() {
+    int originCoor = car.getCoordinate();
+    car.moveForward(1);
+    assertThat(car.getCoordinate()).isEqualTo(originCoor+1);
+  }
+
+  @Test
+  public void 자동차_현재_위치를_String형태로_리턴하는지_확인() {
+    Car testCar = new Car(0,5);
+    assertThat(testCar.coordinateToStr()).isEqualTo("-----");
+
+  }
 }
+
