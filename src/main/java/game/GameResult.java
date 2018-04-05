c548ab6bd31724243f6155fb52787ef9b4964aac
KangHyunGu <hkkang@woowahan.com>
2018-04-05 13:14:06 +0900

    자동차 경주 게임 2단계

--- src/main/java/game/GameResult.java
+++ src/main/java/game/GameResult.java
@@ -1,30 +1,56 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class GameResult {
 
-    private List<List<Car>> histories;
+    private List<List<Car>> records;
 
-    public GameResult(List<List<Car>> histories) {
-        this.histories = histories;
+    public GameResult() {
+        records = new ArrayList<>();
+    }
+
+    public GameResult(List<List<Car>> records) {
+        this.records = records;
+    }
+
+    public void record(List<Car> cars) {
+        records.add(cars);
     }
 
     public String draw() {
-        if(histories == null) {
+        if(records == null) {
             throw new IllegalArgumentException();
         }
 
         StringBuilder result = new StringBuilder();
-        histories.forEach(cars -> {
+        records.forEach(cars -> {
             cars.forEach(car -> {
-                for (int i = 0; i <= car.getPosition(); i++) {
-                    result.append("-");
-                }
+                result.append(car.getName() + " : ");
+                IntStream.rangeClosed(0, car.getPosition())
+                        .forEach(i -> result.append("-"));
+
                 result.append("\n");
             });
             result.append("\n");
         });
         return result.toString();
+    }
+
+    public List<Car> winner() {
+        List<Car> last = new ArrayList<>(records.get(records.size() - 1));
+        Integer top = last.stream()
+                        .max(Comparator.comparing(Car::getPosition))
+                        .get()
+                        .getPosition();
+
+        return last.stream()
+                .filter(car -> Objects.equals(car.getPosition(), top))
+                .collect(Collectors.toList());
     }
 }
