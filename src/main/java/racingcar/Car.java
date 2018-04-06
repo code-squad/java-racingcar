69417b700e6ce7d1869e72858ccf28184b3c1ef1
BokDaHoon <BokDaHoon>
2018-04-06 13:46:13 +0900

    2018-04-06 step2 커밋 초기화 및 업데이트

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -4,21 +4,28 @@ public class Car {
     private String carName;
     private int moveDistance;
 
-    public String getCarName() {
-        return carName;
+    public Car(String carName) {
+        this.carName = carName;
     }
 
-    public Car setCarName(String carName) {
+    public Car(String carName, int moveDistance) {
         this.carName = carName;
-        return this;
+        this.moveDistance = moveDistance;
+    }
+
+    public String getCarName() {
+        return carName;
     }
 
     public int getMoveDistance() {
         return moveDistance;
     }
 
-    public Car setMoveDistance(int moveDistance) {
-        this.moveDistance = moveDistance;
-        return this;
+    public void moveCar() {
+        this.moveDistance++;
+    }
+
+    public boolean matchDistance(int topDistance) {
+        return this.moveDistance == topDistance;
     }
 }
--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -3,7 +3,6 @@ package racingcar;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,8 +16,8 @@ public class RacingGameTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void 생성자_파라미터_차갯수0체크_확인() {
-        List<Car> carList = new ArrayList<Car>();
-        RacingGame racingGame = new RacingGame(carList);
+        List<Car> cars = new ArrayList<Car>();
+        RacingGame racingGame = new RacingGame(cars);
     }
 
     @Test
@@ -41,52 +40,50 @@ public class RacingGameTest {
 
     @Test
     public void 승자1명_테스트() {
-        List<Car> carList = new ArrayList<>();
-        carList.add(new Car().setCarName("test1"));
-        carList.add(new Car().setCarName("test2"));
-        carList.add(new Car().setCarName("test3"));
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("test1"));
+        cars.add(new Car("test2"));
+        cars.add(new Car("test3"));
 
-        GameResult gameResult = new GameResult();
-        gameResult.setCarList(carList);
+        RacingGame rc = new RacingGame(cars);
 
-        gameResult.moveCar(0);
-        gameResult.moveCar(0);
-        gameResult.moveCar(0);
-        gameResult.moveCar(1);
-        gameResult.moveCar(1);
-        gameResult.moveCar(2);
+        rc.moveCar(0);
+        rc.moveCar(0);
+        rc.moveCar(0);
+        rc.moveCar(1);
+        rc.moveCar(1);
+        rc.moveCar(2);
 
-        List<String> result = gameResult.getCarRacingWinner();
+        List<String> result = rc.getCarRacingWinner();
 
         assertThat(printWinner(result)).isEqualTo("test1");
     }
 
     @Test
     public void 승자n명_테스트() {
-        List<Car> carList = new ArrayList<>();
-        carList.add(new Car().setCarName("test1"));
-        carList.add(new Car().setCarName("test2"));
-        carList.add(new Car().setCarName("test3"));
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("test1"));
+        cars.add(new Car("test2"));
+        cars.add(new Car("test3"));
 
-        GameResult gameResult = new GameResult();
-        gameResult.setCarList(carList);
+        RacingGame rc = new RacingGame(cars);
 
-        gameResult.moveCar(0);
-        gameResult.moveCar(1);
-        gameResult.moveCar(2);
+        rc.moveCar(0);
+        rc.moveCar(1);
+        rc.moveCar(2);
 
-        List<String> result = gameResult.getCarRacingWinner();
+        List<String> result = rc.getCarRacingWinner();
 
         assertThat(printWinner(result)).isEqualTo("test1, test2, test3");
     }
 
-    private String printWinner(List<String> winnerList) {
-        int winnerListLength = winnerList.size();
+    private String printWinner(List<String> winners) {
+        int winnersLength = winners.size();
         StringBuilder sb = new StringBuilder();
 
-        for (int index = 0; index < winnerListLength; index++) {
-            sb.append(winnerList.get(index));
-            if (index != winnerListLength - 1) {
+        for (int index = 0; index < winnersLength; index++) {
+            sb.append(winners.get(index));
+            if (index != winnersLength - 1) {
                 sb.append(", ");
             }
         }
