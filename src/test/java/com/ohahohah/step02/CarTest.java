f816ab15ac8c9ed680e3c152551436814476d8a0
SiYoungOh(ohahohah) <ohahohah.dev@gmail.com>
2018-04-06 21:24:02 +0900

    #245 설계에 맞는 생성자 사용으로 변경

--- src/test/java/com/ohahohah/step02/CarTest.java
+++ src/test/java/com/ohahohah/step02/CarTest.java
@@ -21,7 +21,7 @@ public class CarTest {
 
   @Test
   public void 숫자에_따라서_전진횟수가_출력되는지_확인() {
-    Car car = new Car();
+    Car car = new Car("testCar",0);
     assertThat(car.stopOrGo(0)).isEqualTo(0);
     assertThat(car.stopOrGo(4)).isEqualTo(1);
     assertThat(car.stopOrGo(9)).isEqualTo(1);
@@ -29,7 +29,7 @@ public class CarTest {
 
   @Test
   public void 입력값에_따라_자동차_위치가_변화하는지_확인() {
-    Car car = new Car();
+    Car car = new Car("testCar",0);
     int originCoor = car.getCoords();
     car.move(1);
     assertThat(car.getCoords()).isEqualTo(originCoor + 1);
