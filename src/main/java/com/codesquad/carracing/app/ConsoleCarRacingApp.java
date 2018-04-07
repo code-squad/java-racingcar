952958ed7c35b62a796db4efd481cb6d4dc30a0c
Jinho Yim <jinhoyim@outlook.com>
2018-04-07 09:51:56 +0900

    코드 정리

--- src/main/java/com/codesquad/carracing/app/ConsoleCarRacingApp.java
+++ src/main/java/com/codesquad/carracing/app/ConsoleCarRacingApp.java
@@ -21,32 +21,9 @@ public class ConsoleCarRacingApp {
 
         final CarRacing racing = new CarRacing(numberOfCars);
 
-        //for(int i=0; i)
-
-
-//        final Scanner scanner = new Scanner(System.in);
-//
-//        System.out.println("자동차 대수는 몇 대 인가요?");
-//        final String inputNumberOfCar = scanner.nextLine();
-//        final int numberOfCar = Integer.parseInt(inputNumberOfCar);
-//
-//        System.out.println("시도할 회수는 몇 회 인가요?");
-//        final String inputNumberOfTries = scanner.nextLine();
-//        final int numberOfTries = Integer.parseInt(inputNumberOfTries);
-//
-//        final Random random = new Random();
-//        final RandomGenerator randomGenerator = new BoundedRandomGenerator(10, random);
-//        final MoveStrategy strategy = new RandomMoveStrategy(randomGenerator);
-//
-//        final CarRacing carRacing = new CarRacing(numberOfCar, numberOfTries);
-//
-//        List<Car> cars;
-//
-//        for (int i = 0; i < numberOfTries; i++) {
-//            cars = carRacing.getCars();
-//            ConsoleCarRacingApp.printCars(cars);
-//            carRacing.nextTry(strategy);
-//        }
+        for (int i = 0; i < numberOfTries; i++) {
+            racing.nextTry(strategy);
+        }
     }
 
     public static InputView createInputView() {
--- src/main/java/com/codesquad/carracing/domain/CarRacing.java
+++ src/main/java/com/codesquad/carracing/domain/CarRacing.java
@@ -6,24 +6,16 @@ import java.util.List;
 public class CarRacing {
 
     final List<Car> cars;
-    //final int numberOfTries;
-
-    //public CarRacing(final int numberOfCars, final int numberOfTries) {
+    
     public CarRacing(final int numberOfCars) {
         if (numberOfCars == 0) {
             throw new IllegalArgumentException("최소 1대 이상의 자동차가 필요합니다.");
         }
 
-//        if (numberOfTries == 0) {
-//            throw new IllegalArgumentException("1번 이상의 시도 횟수가 필요합니다.");
-//        }
-
         cars = new ArrayList<>();
         for (int i = 0; i < numberOfCars; i++) {
             cars.add(new Car());
         }
-
-        //this.numberOfTries = numberOfTries;
     }
 
     public List<Car> getCars() {
