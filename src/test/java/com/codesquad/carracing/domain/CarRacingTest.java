51a22f2b4ebbc08db9ed57a4994f48ad11e8423e
Jinho Yim <jinhoyim@outlook.com>
2018-04-08 12:51:02 +0900

    FakeRandomGenerator를 람다로 교체

--- src/test/java/com/codesquad/carracing/domain/CarRacingTest.java
+++ src/test/java/com/codesquad/carracing/domain/CarRacingTest.java
@@ -25,46 +25,96 @@ public class CarRacingTest {
 
     @Test
     public void 레이싱_자동차_1대_생성_() {
-        final CarRacing carRacing = new CarRacing(1);
-        final List<Car> cars = carRacing.getCars();
+        final CarRacing racing = new CarRacing(1);
+        final List<Car> cars = racing.getCars();
         assertThat(cars.size()).isEqualTo(1);
     }
 
     @Test
     public void 레이싱_자동차_10대_생성() {
-        final CarRacing carRacing = new CarRacing(10);
-        final List<Car> cars = carRacing.getCars();
+        final CarRacing racing = new CarRacing(10);
+        final List<Car> cars = racing.getCars();
         assertThat(cars.size()).isEqualTo(10);
     }
 
     @Test
-    public void 첫번째_자동차_이동1회() {
-        final CarRacing carRacing = new CarRacing(1);
-        final RandomGenerator generator = new FakeRandomGenerator(RandomMoveStrategy.MOVABLE_MIN);
-        final MoveStrategy strategy = new RandomMoveStrategy(generator);
-        carRacing.nextTry(strategy);
-        final List<Car> cars = carRacing.getCars();
-        assertThat(cars.get(0).getPosition()).isEqualTo(1);
+    public void 레이싱_차량_시작위치() {
+        final CarRacing racing = new CarRacing(1);
+        final int position = racing.getCar(0).getPosition();
+        assertThat(position).isEqualTo(0);
     }
 
     @Test
-    public void 첫번째_자동차_이동_실패() {
-        final CarRacing carRacing = new CarRacing(1);
-        final RandomGenerator generator = new FakeRandomGenerator(RandomMoveStrategy.MOVABLE_MIN - 1);
-        final MoveStrategy strategy = new RandomMoveStrategy(generator);
-        carRacing.nextTry(strategy);
-        final List<Car> cars = carRacing.getCars();
-        assertThat(cars.get(0).getPosition()).isEqualTo(0);
+    public void 이동가능_이동() {
+        final CarRacing racing = new CarRacing(1);
+
+        racing.nextTry(() -> true);
+
+        final Car car = racing.getCar(0);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    public void 이동불가능_정지() {
+        final CarRacing racing = new CarRacing(1);
+
+        racing.nextTry(() -> false);
+
+        final Car car = racing.getCar(0);
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+
+    @Test
+    public void 랜덤최소값일때_시도() {
+        final CarRacing racing = new CarRacing(1);
+        final MoveStrategy strategy = new RandomMoveStrategy(() -> RandomMoveStrategy.MOVABLE_MIN);
+
+        racing.nextTry(strategy);
+
+        final Car car = racing.getCar(0);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    public void 랜덤최소값보다_작은_범위밖() {
+        final CarRacing racing = new CarRacing(1);
+        final MoveStrategy strategy = new RandomMoveStrategy(() -> RandomMoveStrategy.MOVABLE_MIN - 1);
+
+        racing.nextTry(strategy);
+
+        final Car car = racing.getCar(0);
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+
+    @Test
+    public void 랜덤최대값일때_시도() {
+        final CarRacing racing = new CarRacing(1);
+        final MoveStrategy strategy = new RandomMoveStrategy(() -> RandomMoveStrategy.MOVABLE_MAX);
+
+        racing.nextTry(strategy);
+
+        final Car car = racing.getCar(0);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    public void 랜덤최대값보다_큰_범위밖() {
+        final CarRacing racing = new CarRacing(1);
+        final MoveStrategy strategy = new RandomMoveStrategy(() -> RandomMoveStrategy.MOVABLE_MAX + 1);
+
+        racing.nextTry(strategy);
+
+        final Car car = racing.getCar(0);
+        assertThat(car.getPosition()).isEqualTo(0);
     }
 
     @Test
     public void 두번째_자동차_이동2회() {
-        final CarRacing carRacing = new CarRacing(2);
-        final RandomGenerator generator = new FakeRandomGenerator(RandomMoveStrategy.MOVABLE_MIN);
-        final MoveStrategy strategy = new RandomMoveStrategy(generator);
-        carRacing.nextTry(strategy);
-        carRacing.nextTry(strategy);
-        final List<Car> cars = carRacing.getCars();
+        final CarRacing racing = new CarRacing(2);
+        final MoveStrategy strategy = () -> true;
+        racing.nextTry(strategy);
+        racing.nextTry(strategy);
+        final List<Car> cars = racing.getCars();
         assertThat(cars.get(1).getPosition()).isEqualTo(2);
     }
 }
--- src/test/java/com/codesquad/carracing/domain/RandomMoveStrategyTest.java
+++ src/test/java/com/codesquad/carracing/domain/RandomMoveStrategyTest.java
@@ -7,36 +7,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RandomMoveStrategyTest {
     @Test
     public void 최소값_이동가능() {
-        final RandomGenerator generator = new FakeRandomGenerator(
-                RandomMoveStrategy.MOVABLE_MIN);
-        final MoveStrategy strategy = new RandomMoveStrategy(generator);
+        final MoveStrategy strategy = new RandomMoveStrategy(() -> RandomMoveStrategy.MOVABLE_MIN);
         final boolean isMovable = strategy.isMovable();
         assertThat(isMovable).isEqualTo(true);
     }
 
     @Test
     public void 최소값보다_작을때() {
-        final RandomGenerator generator = new FakeRandomGenerator(
-                RandomMoveStrategy.MOVABLE_MIN - 1);
-        final MoveStrategy strategy = new RandomMoveStrategy(generator);
+        final MoveStrategy strategy = new RandomMoveStrategy(() -> RandomMoveStrategy.MOVABLE_MIN - 1);
         final boolean isMovable = strategy.isMovable();
         assertThat(isMovable).isEqualTo(false);
     }
 
     @Test
     public void 최대값_이동가능() {
-        final RandomGenerator generator = new FakeRandomGenerator(
-                RandomMoveStrategy.MOVABLE_MAX);
-        final MoveStrategy strategy = new RandomMoveStrategy(generator);
+        final MoveStrategy strategy = new RandomMoveStrategy(() -> RandomMoveStrategy.MOVABLE_MAX);
         final boolean isMovable = strategy.isMovable();
         assertThat(isMovable).isEqualTo(true);
     }
 
     @Test
     public void 최대값보다_클때() {
-        final RandomGenerator generator = new FakeRandomGenerator(
-                RandomMoveStrategy.MOVABLE_MAX + 1);
-        final MoveStrategy strategy = new RandomMoveStrategy(generator);
+        final MoveStrategy strategy = new RandomMoveStrategy(() -> RandomMoveStrategy.MOVABLE_MAX + 1);
         final boolean isMovable = strategy.isMovable();
         assertThat(isMovable).isEqualTo(false);
     }
