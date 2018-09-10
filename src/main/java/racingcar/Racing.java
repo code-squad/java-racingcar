756fbab3915ec60284a7303d7ab75d6caa70b9aa
Brad Lee <brad903@naver.com>
2018-09-10 18:05:09 +0900

    implmenting racingGame step2

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -16,25 +16,36 @@ public class Racing {
     }
 
     public void run() {
-        Random random = new Random();
-
-        for(int i = 0; i < time; i++){
-            for(int j = 0; j < carNum; j++){
-                int posAdder = random.nextInt(10);
-                if(posAdder >= 4){
-                    carPositions.set(j, carPositions.get(j) + 1);
-                }
-            }
+        for (int i = 0; i < time; i++) {
+            selectCar(i);
         }
     }
 
-    public void print(){
+    public void selectCar(int i) {
+        for (int j = 0; j < carNum; j++) {
+            posAdd(j);  // move each car
+        }
+    }
+
+    public void posAdd(int j) {
+        Random random = new Random();
+        int posAdder = random.nextInt(10);
+        if (posAdder >= 4) {
+            carPositions.set(j, carPositions.get(j) + 1);
+        }
+    }
+
+    public void print() {
         System.out.println("실행 결과");
-        for(int i = 0; i < carPositions.size(); i++){
-            for(int j = 0; j < carPositions.get(i); j++){
-                System.out.print("-");
-            }
+        for (int i = 0; i < carPositions.size(); i++) {
+            showCarPos(i);
             System.out.println();
+        }
+    }
+
+    public void showCarPos(int i) {
+        for (int j = 0; j < carPositions.get(i); j++) {
+            System.out.print("-");
         }
     }
 
@@ -42,10 +53,15 @@ public class Racing {
         Scanner scan = new Scanner(System.in);
 
         Racing racing = new Racing();
-        System.out.println("자동차 대수는 몇 대 인가요? ");
-        racing.carNum = scan.nextInt();
-        System.out.println("시도할 회수는 몇 회 인가요?");
-        racing.time = scan.nextInt();
+
+        try{
+            System.out.println("자동차 대수는 몇 대 인가요? ");
+            racing.carNum = scan.nextInt();
+            System.out.println("시도할 회수는 몇 회 인가요?");
+            racing.time = scan.nextInt();
+        }catch(Exception e){
+            return;
+        }
 
         racing.init();
         racing.run();
