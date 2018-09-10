f5381824cd171269ed1848665dcb56fc5cbbff39
sechun <sechun0215@gmail.com>
2018-09-10 16:38:25 +0900

    indent modified

--- src/main/java/racingcar/Main.java
+++ src/main/java/racingcar/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("자동차 대 수는 몇대인가요?");
         int carNum = sc.nextInt();
         System.out.println("시도할 횟 수는 몇번인가요?");
@@ -14,6 +15,5 @@ public class Main {
 
         Racing race = new Racing(time, carNum);
         race.run();
-
     }
 }
