21f9772d2b722e1488004548973d26c70669e8c3
jar100 <39078045+jar100@users.noreply.github.com>
2018-09-11 15:45:43 +0900

    스탭1 구현 (#290)
    * 스탭1 구현
    
    * 스탭1 수정

--- src/main/java/RacingGame.java
+++ src/main/java/RacingGame.java
@@ -1,5 +1,45 @@
+import java.util.*;
+
+
 public class RacingGame {
+    static final int MOVE_CAR = 4;
+    static final int MAXIMUM_NUM = 10;
+
     public static void main(String[] args) {
-        System.out.println("hollo world");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        int cars = scanner.nextInt();
+        System.out.println("시도할 횟수는 몇번인가요?");
+        int movingCount = scanner.nextInt();
+        int[] carArrays = makeCarsList(cars, movingCount);
+        System.out.println("실행 결과");
+        displayCar(carArrays, movingCount);
+
+        // 배열값 확인용
+        //System.out.println(Arrays.toString(carArrays));
     }
+
+    //cars 와 movingCount를 곱해서 1차원 배열로만듬
+    public static int[] makeCarsList(int cars, int movingCount) {
+        int[] carArrays = new int[cars * movingCount];
+        Random random = new Random();
+        for (int i = 0; i < cars * movingCount; i++) {
+            carArrays[i] = random.nextInt(MAXIMUM_NUM);
+        }
+        return carArrays;
+    }
+
+    // 자동차 배열을 하나 읽어 출력
+    public static void displayCar(int[] cars, int movingCount) {
+        for (int i = 0; i < cars.length; i++) {
+            if (i != 0 && i % movingCount == 0) {
+                System.out.println("");
+            }
+            if (cars[i] >= MOVE_CAR) {
+                System.out.print("-");
+            }
+        }
+        System.out.println("");
+    }
+
 }
