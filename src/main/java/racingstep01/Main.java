b5a9ffd868fe6e0ac17f1ed91fe94422a09cf2a8
lkhlkh23 <lkhlkh09@gmail.com>
2018-09-11 14:33:41 +0900

    Make a step1

---
+++ src/main/java/racingstep01/Main.java
@@ -0,0 +1,15 @@
+package racingstep01;
+
+public class Main {
+    public static void main(String[] args) {
+        Inputs inputs = new Inputs();
+        int time = inputs.getTime();
+        int cars = inputs.getCars();
+
+        inputs.close();
+
+        Racing racing = new Racing(time, cars);
+        racing.run();
+        racing.printRacingResult();
+    }
+}
