4a45c60b2f11a41f96091164a11505d609c7b640
zingoworks <zingoworks@gmail.com>
2018-09-11 17:12:05 +0900

    update : step2

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -4,69 +4,72 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class RacingGame {
+    private static final int Condition = 4;
+    private static final int Bound = 10;
 
-    static int[] carPositions;
-    static int condition = 4;
-    static int bound = 10;
+    private int[] carPositions;
 
-    public static int getVehicles(Scanner scanner) {
+    public static void main(String[] args) {
+        RacingGame racingGame = new RacingGame();
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int vehicles = racingGame.getVehicles(scanner);
+        int trials = racingGame.getTrials(scanner);
+
+        racingGame.play(racingGame, random, vehicles, trials);
+    }
+
+    public void play(RacingGame racingGame, Random random, int vehicles, int trials) {
+        racingGame.setCarpositions(vehicles);
+        racingGame.run(random, vehicles, trials);
+        racingGame.print(vehicles);
+    }
+
+    public int getVehicles(Scanner scanner) {
         System.out.println("자동차 대 수는 몇 대 인가요?");
-        // int getVehicles로 변수명을 통일하는게 좋을 지 궁금합니다.
         int numberOfVehicles = scanner.nextInt();
         return numberOfVehicles;
     }
 
-    public static int getTrials(Scanner scanner) {
+    public int getTrials(Scanner scanner) {
         System.out.println("시도 할 회수는 몇 회 인가요?");
         int numberOfTrials = scanner.nextInt();
         return numberOfTrials;
     }
 
-    public static int[] setCarpositions(int vehicles) {
+    public int[] setCarpositions(int vehicles) {
         carPositions = new int[vehicles];
         return carPositions;
     }
 
-    public static void run(Random random, int vehicles, int trials) {
+    public void run(Random random, int vehicles, int trials) {
         for (int i = 0; i < trials; i++) {
             runByVehicles(random, vehicles);
         }
     }
 
-    public static void runByVehicles(Random random, int vehicles) {
+    public void runByVehicles(Random random, int vehicles) {
         for (int i = 0; i < vehicles; i++) {
             runByCondition(random, i);
         }
     }
 
-    public static void runByCondition(Random random, int i) {
-        //원래 범위와 조건 각각 10, 4로 넣었는데 강의시간에 하드코딩X의 케이스를 참고해서 바꿔봤습니다.
-        if (random.nextInt(bound) >= condition) {
+    public void runByCondition(Random random, int i) {
+        if (random.nextInt(Bound) >= Condition) {
             carPositions[i]++;
         }
     }
 
-    public static String repeat(String str, int times) {
+    public String repeat(String str, int times) {
         return new String(new char[times]).replace("\0", str);
     }
 
-    public static void print(int vehicles) {
+    public void print(int vehicles) {
         System.out.println("실행 결과");
         for (int i = 0; i < vehicles; i++) {
             String result = repeat("-", carPositions[i]);
             System.out.println(result);
         }
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-
-        int vehicles = getVehicles(scanner);
-        int trials = getTrials(scanner);
-
-        setCarpositions(vehicles);
-        run(random, vehicles, trials);
-        print(vehicles);
     }
 }
