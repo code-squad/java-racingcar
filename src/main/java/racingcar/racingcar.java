30c8e88e2fb6e4ea6555a2fc7b7e8e4374e42b2c
foreverever <lollolzkk@gmail.com>
2018-09-11 16:07:09 +0900

    step1 complete! (#298)

--- src/main/java/racingcar/racingcar.java
+++ src/main/java/racingcar/racingcar.java
@@ -1,8 +1,72 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Random;
+
 public class racingcar {
+    static Scanner sc;
+
+    //자동차 대수 입력 받기
+    public static int getCarNum() {
+        sc = new Scanner(System.in);
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        int carNum = sc.nextInt();
+        return carNum;
+    }
+
+    //시도 회수 입력 받기
+    public static int getTryNum() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        int tryNum = sc.nextInt();
+        sc.close();
+        return tryNum;
+    }
+
+    //랜덤값 받기
+    public static int getRandNum() {
+        Random random = new Random();
+        int randNum = random.nextInt(10);
+        return randNum;
+    }
+
+    //이동 가능 여부 체크
+    public static boolean isPossibleMove() {
+        final int minRandNum = 4;
+        if (getRandNum() >= minRandNum) {
+            return true;
+        }
+        return false;
+    }
+
+    //경주하기
+    public static List<String> run(int carNum, int tryNum) {
+        List<String> result = new ArrayList();
+        for (int i = 0; i < carNum; i++) {
+            String str = "";
+            for (int j = 0; j < tryNum; j++) {
+                if (isPossibleMove()) {
+                    str += "-";
+                }
+            }
+            result.add(str);
+        }
+        return result;
+    }
+
+    //결과화면 출력
+    public static void printResult(List<String> result) {
+        for (int i = 0; i < result.size(); i++) {
+            System.out.println(result.get(i));
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("hello world");
-        
+        List<String> result = new ArrayList();
+        int carNum = getCarNum();
+        int tryNum = getTryNum();
+        result = run(carNum, tryNum);
+        printResult(result);
     }
 }
