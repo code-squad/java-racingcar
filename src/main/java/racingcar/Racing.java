bf55d91297570b15e8d37f1c33d71ecf6a208f89
Brad Lee <33172368+brad903@users.noreply.github.com>
2018-09-11 14:10:40 +0900

    자동차 경주 2단계 구현하기 (#271)
    * implmenting racingGame step2
    
    * revise feedback lv2

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -9,6 +9,11 @@ public class Racing {
     private int carNum;
     private ArrayList<Integer> carPositions = new ArrayList<>();
 
+    public Racing(int time, int carNum) {
+        this.time = time;
+        this.carNum = carNum;
+    }
+
     public void init() {
         for (int i = 0; i < carNum; i++) {
             carPositions.add(1);
@@ -16,37 +21,53 @@ public class Racing {
     }
 
     public void run() {
-        Random random = new Random();
-
-        for(int i = 0; i < time; i++){
-            for(int j = 0; j < carNum; j++){
-                int posAdder = random.nextInt(10);
-                if(posAdder >= 4){
-                    carPositions.set(j, carPositions.get(j) + 1);
-                }
-            }
+        for (int i = 0; i < time; i++) {
+            selectCar(i);
         }
     }
 
-    public void print(){
+    public void selectCar(int i) {
+        for (int j = 0; j < carNum; j++) {
+            posAdd(j);  // move each car
+        }
+    }
+
+    public void posAdd(int j) {
+        Random random = new Random();
+        int posAdder = random.nextInt(10);
+        if (posAdder >= 4) {
+            carPositions.set(j, carPositions.get(j) + 1);
+        }
+    }
+
+    public void print() {
         System.out.println("실행 결과");
-        for(int i = 0; i < carPositions.size(); i++){
-            for(int j = 0; j < carPositions.get(i); j++){
-                System.out.print("-");
-            }
+        for (int i = 0; i < carPositions.size(); i++) {
+            showCarPos(i);
             System.out.println();
+        }
+    }
+
+    public void showCarPos(int i) {
+        for (int j = 0; j < carPositions.get(i); j++) {
+            System.out.print("-");
         }
     }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int carNum, time;
 
-        Racing racing = new Racing();
-        System.out.println("자동차 대수는 몇 대 인가요? ");
-        racing.carNum = scan.nextInt();
-        System.out.println("시도할 회수는 몇 회 인가요?");
-        racing.time = scan.nextInt();
+        try{
+            System.out.println("자동차 대수는 몇 대 인가요? ");
+            carNum = scan.nextInt();
+            System.out.println("시도할 회수는 몇 회 인가요?");
+            time = scan.nextInt();
+        }catch(Exception e){
+            return;
+        }
 
+        Racing racing = new Racing(time, carNum);  // 피드백 반영 → 객체 외부에서 상태값 변경하지 않도록
         racing.init();
         racing.run();
         racing.print();
