d344b7ee939565778e09318b6b411b916cd186bf
geonHee <corqkrtk3@naver.com>
2018-09-12 22:41:30 +0900

    step2 method division

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -4,35 +4,45 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class RacingGame {
-    public static void run(){
+
+    public static void main(String[] args) {
+        int[] carPositions = makeCar();
+        run(carPositions);
+    }
+
+    public static int[] makeCar(){
         System.out.println("자동차 대수는 몇 대 인가요?");
         Scanner s = new Scanner(System.in);
         int carNum = s.nextInt();
+        int[] carPositions = new int[carNum];
+        return carPositions;
+    }
+
+    public static void run(int[] carPositions) {
+        Scanner s = new Scanner(System.in);
         System.out.println("시도할 횟수는 몇 회 인가요?");
         int count = s.nextInt();
-
-        int[] carPositions = new int[carNum];
-
         Random rnd = new Random();
-
-        for(int i = 0; i < carNum; i++){
+        for (int i = 0; i < carPositions.length; i++) {
             carPositions[i] = 0;
-            for(int j = 0; j < count; j++){
+            for (int j = 0; j < count; j++) {
                 int move = rnd.nextInt(10);
-                if(move >= 4)
+                if (move >= 4)
                     carPositions[i] += 1;
             }
         }
+        output(carPositions);
+    }
 
-        for(int i = 0; i < carNum; i++){
-            for(int j = 0; j < carPositions[i]; j++){
+
+    public static void output(int[] carPositions){
+        for (int i = 0; i < carPositions.length; i++) {
+            for (int j = 0; j < carPositions[i]; j++) {
                 System.out.print("-");
             }
-            System.out.println();
+            if(i < carPositions.length -1)
+                System.out.println();
         }
     }
-
-    public static void main(String[] args){
-        run();
-    }
 }
+
