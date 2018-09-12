e43e0515fb5b16e1d657ee7e072a5a8241dc0624
LYJr <lyi91@naver.com>
2018-09-12 15:49:34 +0900

    step1

--- src/main/java/step1/racingcar.java
+++ src/main/java/step1/racingcar.java
@@ -1,68 +1,59 @@
 package step1;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class racingcar {
 
-static Scanner s= new Scanner(System.in);
-static Random r = new Random();
+    static Scanner s = new Scanner(System.in);
 
-public static void main(String[] args) {
-int car = 0;
-int move = 0;
+    public static void main(String[] args) {
+        int car = 0;
+        int carmove[] = new int[3];
 
-String behavior = "";
+        car = carNumber();
+        carmove = moveNumber();
 
-car = carSn(car);
-move = carMove(move);
+        System.out.println("************");
+        for (int i = 0; i < car; i++) {
+            moveRandom(carmove);
+            out(carmove);
+            System.out.println();
+        }
+    }
 
-System.out.println("================");
-System.out.println("");
+    static int carNumber() {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        int car = s.nextInt();
 
-for(int i=0; i<car; i++) {
-behavior = run(car, move);
-}
-}
+        return car;
+    }
 
-public static int carSn(int car) {                          //자동차의 숫자를 입력받는 메서드
-int cars = 0;
+    static int[] moveNumber() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        int[] action = new int[s.nextInt()];
 
-System.out.println("자동차의 숫자를 입력하시오.");
-cars = s.nextInt();
+        return action;
+    }
 
-return cars;
-}//End carSn
+    static int[] moveRandom(int[] carmove) {
+        Random r = new Random();
+        for (int i = 0; i < carmove.length; i++) {
+            carmove[i] = r.nextInt(9);
+        }
 
-public static int carMove(int move) {                       //자동차를 움직일 횟수를 입력받는 메서드
-int carrun = 0;
+        //     System.out.println(Arrays.toString(carmove)); ->확인용
+        return carmove;
+    }
 
-System.out.println("자동차를 움직일 횟수를 입력하시오.");
-carrun = s.nextInt();
-
-return carrun;
-}//End carMove
-
-public static String run(int car, int move) {               //자동차의 이동값을 구하는 횟수
-ArrayList<Integer> array = new ArrayList<>();
-String k= "";
-
-while(array.size() < move) {
-    array.add(r.nextInt(9));
-/*	for(Integer ar : array) {		-> 확인용
-        System.out.print(ar);
-    } */
-}
-
-for(int i=0; i<move; i++){
-    if(array.get(i) >=4) {
-        System.out.print("-");
+    static void out(int[] carmove) {
+        for (int i = 0; i < carmove.length; i++) {
+            if (carmove[i] >= 4) {
+                System.out.print("-");
+            } else {
+                System.out.print("");
+            }
+        }
     }
 }
-
-System.out.println();
-return k;
-
-} //End run
-} //End class
