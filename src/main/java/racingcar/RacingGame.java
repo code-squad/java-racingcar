65a27b25c7ea1e154ed8fa4faab09f017bd824a5
Choi-dongsun <40771864+Choi-dongsun@users.noreply.github.com>
2018-09-13 12:09:03 +0900

    step1 구현 (#309)

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -2,6 +2,7 @@ package racingcar;
 
 public class RacingGame {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Racing racing = new Racing();
+        racing.run();
     }
 }
