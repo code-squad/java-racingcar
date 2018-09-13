4bb941e8ab4181e660db822ed6829df28b58757e
Choi-dongsun <40771864+Choi-dongsun@users.noreply.github.com>
2018-09-13 14:18:10 +0900

    step2 구현 (#311)

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -4,51 +4,62 @@ import java.util.Scanner;
 
 public class Racing {
 
+    private static final int CONDITION = 4;
+    private static final String DASH = "-";
+
     private int time;
     private int[] carPositions;
 
-    private final int CONDITION = 4;
-
-    void run() {
+    public void run() {
         inputData();
         move();
         print();
     }
 
-    void inputData() {
+    private void inputData() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("자동차 대수는 몇 대 인가요?");
-        int numOfCar = scanner.nextInt();
-        carPositions = new int[numOfCar];
+        carPositions = new int[scanner.nextInt()];
 
         System.out.println("시도할 회수는 몇 회 인가요?");
         time = scanner.nextInt();
     }
 
-    int movingJudge() {
+    private static int movingJudge() {
         Random random = new Random();
-        int preCondition = (random.nextInt(10) - 1);
 
-        return preCondition;
+        return (random.nextInt(10) - 1);
     }
 
-    void move() {
+    private void move() {
         for (int i = 1; i < time + 1; i++) {
-            for (int j = 0; j < carPositions.length; j++) {
-                if (movingJudge() > CONDITION) {
-                    carPositions[j]++;
-                }
-            }
+            moveCount();
         }
     }
 
-    void print() {
+    private void moveCount() {
         for (int i = 0; i < carPositions.length; i++) {
-            for (int j = 0; j < carPositions[i]; j++) {
-                System.out.printf("-");
-            }
-            System.out.println("");
+            moveDecide(i);
+        }
+    }
+
+    private void moveDecide(int i) {
+        if (movingJudge() > CONDITION) {
+            carPositions[i]++;
+        }
+    }
+
+    private void print() {
+        for (int i = 0; i < carPositions.length; i++) {
+            printImagery(carPositions[i]);
+            System.out.println();
+        }
+    }
+
+    private void printImagery(int carPosition) {
+        for (int i = 0; i < carPosition; i++) {
+            System.out.printf(DASH);
         }
     }
 }
