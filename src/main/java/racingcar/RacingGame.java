dfa476fd88bf1eb4763785ea2e687e9beba1802b
geonHee <corqkrtk3@naver.com>
2018-09-13 13:39:14 +0900

    method division

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -4,45 +4,57 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class RacingGame {
+    public static Scanner s = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int[] carPositions = makeCar();
-        run(carPositions);
+        output(time(makeCar()));
     }
 
     public static int[] makeCar(){
         System.out.println("자동차 대수는 몇 대 인가요?");
-        Scanner s = new Scanner(System.in);
-        int carNum = s.nextInt();
-        int[] carPositions = new int[carNum];
+
+        int[] carPositions = new int[s.nextInt()];
         return carPositions;
     }
 
-    public static void run(int[] carPositions) {
-        Scanner s = new Scanner(System.in);
+    public static int[] time(int[] carPositions) {
         System.out.println("시도할 횟수는 몇 회 인가요?");
         int count = s.nextInt();
-        Random rnd = new Random();
         for (int i = 0; i < carPositions.length; i++) {
             carPositions[i] = 0;
-            for (int j = 0; j < count; j++) {
-                int move = rnd.nextInt(10);
-                if (move >= 4)
-                    carPositions[i] += 1;
-            }
+            moveRanCreate(carPositions, count, i);
         }
-        output(carPositions);
+        return carPositions;
     }
 
+    public static void moveRanCreate(int [] carPositions, int count, int i) {
+        Random rnd = new Random();
+        for (int j = 0; j < count; j++) {
+            run(carPositions, rnd.nextInt(10), i);
+        }
+    }
+
+    public static void run(int [] carPositions, int move, int i){
+        if (move >= 4)
+            carPositions[i] += 1;
+    }
 
     public static void output(int[] carPositions){
         for (int i = 0; i < carPositions.length; i++) {
-            for (int j = 0; j < carPositions[i]; j++) {
-                System.out.print("-");
-            }
-            if(i < carPositions.length -1)
-                System.out.println();
+            carDist(carPositions, i);
+            finishLine(carPositions, i);
         }
+    }
+
+    private static void carDist(int[] carPositions, int i) {
+        for (int j = 0; j < carPositions[i]; j++) {
+            System.out.print("-");
+        }
+    }
+
+    private static void finishLine(int[] carPositions, int i) {
+        if(i < carPositions.length -1)
+            System.out.println();
     }
 }
 
