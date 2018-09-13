7918c8d099945cf51b05b2d85f69c922e81feb97
plcker <33652399+gbeea1004@users.noreply.github.com>
2018-09-13 16:09:10 +0900

    Step2 (#312)
    * step2 method division
    
    * method division
    
    * carPositions update to instance variable, time() divisition

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -4,35 +4,60 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class RacingGame {
-    public static void run(){
+    public static Scanner s = new Scanner(System.in);
+    int[] carPositions;
+
+    public static void main(String[] args) {
+        RacingGame g = new RacingGame();
+        g.makeCar();
+        g.initCarPos(g.time());
+        g.output();
+    }
+
+    public void makeCar(){
         System.out.println("자동차 대수는 몇 대 인가요?");
-        Scanner s = new Scanner(System.in);
-        int carNum = s.nextInt();
+        carPositions = new int[s.nextInt()];
+    }
+
+    public int time() {
         System.out.println("시도할 횟수는 몇 회 인가요?");
         int count = s.nextInt();
-
-        int[] carPositions = new int[carNum];
-
-        Random rnd = new Random();
-
-        for(int i = 0; i < carNum; i++){
+        return count;
+    }
+    public void initCarPos(int count){
+        for (int i = 0; i < carPositions.length; i++) {
             carPositions[i] = 0;
-            for(int j = 0; j < count; j++){
-                int move = rnd.nextInt(10);
-                if(move >= 4)
-                    carPositions[i] += 1;
-            }
+            moveRanCreate(count, i);
         }
-
-        for(int i = 0; i < carNum; i++){
-            for(int j = 0; j < carPositions[i]; j++){
-                System.out.print("-");
-            }
-            System.out.println();
+    }
+    public void moveRanCreate(int count, int i) {
+        Random rnd = new Random();
+        for (int j = 0; j < count; j++) {
+            run(rnd.nextInt(10), i);
         }
     }
 
-    public static void main(String[] args){
-        run();
+    public void run(int move, int i){
+        if (move >= 4)
+            carPositions[i] += 1;
+    }
+
+    public void output(){
+        for (int i = 0; i < carPositions.length; i++) {
+            carDist(i);
+            finishLine(i);
+        }
+    }
+
+    public void carDist(int i) {
+        for (int j = 0; j < carPositions[i]; j++) {
+            System.out.print("-");
+        }
+    }
+
+    public void finishLine(int i) {
+        if(i < carPositions.length -1)
+            System.out.println();
     }
 }
+
---
+++ src/test/java/practice/ExSpace.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class ExSpace {
+    int num = 1;
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 4; i++) {
+            ExSpace car = new ExSpace();
+        }
+
+        System.out.print(car.num);
+    }
+}
