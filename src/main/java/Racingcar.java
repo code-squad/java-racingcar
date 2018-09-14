311adfff64a775811f23bf5a615d4efff2f61a44
daesoop <40817315+daesoop@users.noreply.github.com>
2018-09-14 18:01:34 +0900

    Step2 (#323)
    * commit push test
    
    * step2 다시 한번 올리겠습니다.

--- src/main/java/Racingcar.java
+++ src/main/java/Racingcar.java
@@ -1,48 +1,46 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class Racingcar {
 
-    //차 갯수 생성
+    static final int Possible_Move_Num = 4;
+    static final int Random_Num = 10;
+    // commit test
     public static int carNum() {
-        System.out.println("자동차 몇대를 가져오시겠습니까?");
-        Scanner scan1 = new Scanner(System.in);
-        int carNum = scan1.nextInt();
-        System.out.println(carNum);
-        return carNum;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("자동차 몇 대를 호출하시겠습니까?");
+        int cars = scanner.nextInt();
+        return cars;
     }
 
-    //차 이동 숫자
-    public static int moveNum() {
-        System.out.println("얼마나 이동 하시겠습니까?");
-        Scanner scan2 = new Scanner(System.in);
-        int moveNum = scan2.nextInt();
-        return moveNum;
+    public static int carMove() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("자동차 얼마나 이동하시겠습니까?");
+        int carMove = scanner.nextInt();
+        return carMove;
     }
 
-    // 게임 실행 및 출력
-    public static void playGame(int carN, int move) {
-        Random random = new Random();
-        int randomNum = 0;
-        int[] cars = new int[carN];
-        System.out.println("실행결과");
-        for (int i = 0; i < carN; i++) {
-            cars[i] = randomNum;
-            for (int j = 0; j < move; j++) {
-            randomNum = random.nextInt(10);
-                if (randomNum >= 4) {
-                    System.out.print("-");
-                } else {
-                    System.out.print("");
-                }
-            }
+    public static void playGame(int car, int carMove) {
+        int[] cars = new int[car];
+        for (int i = 0; i < car; i++) {
+            moveNum(carMove, cars, i);
             System.out.println("");
         }
     }
 
+    public static void moveNum(int carMove, int[] cars, int i) {
+        int ran;
+        for (int j = 0; j < carMove; j++) {
+            ran = (int) (Math.random() * Random_Num);
+            cars[i] = ran;
+            if (ran >= Possible_Move_Num) {
+                System.out.print("-");
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        int carN = carNum();
-        int moveN = moveNum();
-        playGame(carN, moveN);
+        int cars = carNum();
+        int carMove = carMove();
+        playGame(cars, carMove);
     }
 }
