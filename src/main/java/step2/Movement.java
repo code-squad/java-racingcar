939e229e6d33e212d907b11f1016c0f0a02bfb54
LYJr <lyi91@naver.com>
2018-09-14 11:19:02 +0900

    stat2 new reflection

--- src/main/java/step2/Movement.java
+++ src/main/java/step2/Movement.java
@@ -1,18 +1,30 @@
 package step2;
+
 import java.util.Random;
 import java.util.Scanner;
 
 public class Movement {
     static Scanner s = new Scanner(System.in);
 
-    RacingCar Rc = new RacingCar();
-    int car = Rc.carNumber();
-    int[] carMove = Rc.moveNumber();
+    int car = 0;
+    int[] carMove = new int[0];
+
+    public int carNumber() {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        return car = s.nextInt();
+    }
+
+    public int[] moveNumber() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        carMove = new int[s.nextInt()];
+        return carMove;
+    }
 
     public void out() {
         System.out.println("***********");
+        System.out.println("차 입력값" + car);
         for (int i = 0; i < car; i++) {
-            run1();
+            run();
             System.out.println();
         }
     }
@@ -25,7 +37,7 @@ public class Movement {
         return carMove;
     }
 
-    public void run1() {
+    public void run() {
         for (int i = 0; i < carMove.length; i++) {
             repetition(carMove[i]);
             moveRandom();
--- src/main/java/step2/RacingCar.java
+++ src/main/java/step2/RacingCar.java
@@ -6,18 +6,10 @@ public class RacingCar {
     static Scanner s = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Movement Mm = new Movement();
-        Mm.out();
-    }
+        Movement mm = new Movement();
 
-    static int carNumber() {
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        return s.nextInt();
-    }
-
-    static int[] moveNumber() {
-        System.out.println("시도할 회수는 몇 회 인가요?");
-        int[] action = new int[s.nextInt()];
-        return action;
+        mm.carNumber();
+        mm.moveNumber();
+        mm.out();
     }
 }
