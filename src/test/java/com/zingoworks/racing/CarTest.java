b7b8f5775b99ab9daebef9fbbd3b2579c207d88f
zingoworks <zingoworks@gmail.com>
2018-09-18 16:55:30 +0900

    update : test codes

--- src/test/java/com/zingoworks/racing/CarTest.java
+++ src/test/java/com/zingoworks/racing/CarTest.java
@@ -11,28 +11,21 @@ public class CarTest {
         int i = 0;
         while (i < 100) {
             Car c1 = new Car("zingo", 0);
-            Car.updatePosition(c1);
-            System.out.println(Car.extractPosition(c1));
+            c1.updatePosition();
+            System.out.println(c1.extractPosition());
             i++;
         }
-
     }
 
     @Test
     public void extractPosition() {
         Car c2 = new Car("zingo", 0);
-        assertThat(Car.extractPosition(c2)).isEqualTo(0);
+        assertThat(c2.extractPosition()).isEqualTo(0);
     }
 
     @Test
     public void extractNameOfCar() {
         Car c3 = new Car("zingo", 0);
-        assertThat(Car.extractNameOfCar(c3)).isEqualTo("zingo");
-    }
-
-    @Test
-    public void extractMaxposition() {
-        Car[] c4 = {new Car("zingo", 0), new Car("pobi", 4)};
-        assertThat(RacingGame.extractMaxposition(c4)).isEqualTo(4);
+        assertThat(c3.extractNameOfCar()).isEqualTo("zingo");
     }
 }
