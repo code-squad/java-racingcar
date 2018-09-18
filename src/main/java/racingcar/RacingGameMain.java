1d047a16917d33e14121fa97bc7741f7abd0a98b
tskim <lollolzkk@gmail.com>
2018-09-18 14:27:17 +0900

    step5 complete

--- src/main/java/racingcar/RacingGameMain.java
+++ src/main/java/racingcar/RacingGameMain.java
@@ -9,7 +9,7 @@ public class RacingGameMain {
 
         rc.makeCar(inputName());                //자동차 생성 및 이름 설정
         rc.run(getTryNum());                    //시도 회수 받아 경주시작
-        printScore(rc.cars);                    //이동 점수(거리) 출력
+        printScore(rc.getCars());               //이동 점수(거리) 출력
         printWinner(rc.findWinners());          //우승자 출력
     }
 }
