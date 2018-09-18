b947f4c473bb95a728b9b409d9c67ae1d38dc1b3
kimse9450@gmail.com <kimse9450@gmail.com>
2018-09-18 14:49:27 +0900

    step5

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -1,33 +1,20 @@
 package racingcar;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class InputView {
 
     public static Scanner scanner = new Scanner(System.in);
 
-    public static ArrayList getCars() {
-        ArrayList<Car> cars;
-
+    public static String getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        cars = createCars(scanner.next());
-        return cars;
+        String carNames = scanner.next();
+        return carNames;
     }
 
-    public static ArrayList createCars(String input) {
-        ArrayList<Car> cars = new ArrayList<>();
-        String[] names = input.split(",");
-
-        for(int i = 0 ; i < names.length; i++) {
-            cars.add(new Car(names[i]));
-        }
-        return cars;
-    }
-
-    public static int getTime() {
+    public static int getTryNo() {
         System.out.println("시도할 횟수는 몇 회 인가요?");
-        int time = scanner.nextInt();
-        return time;
+        int tryNo = scanner.nextInt();
+        return tryNo;
     }
 }
