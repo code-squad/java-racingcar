3b487962957b3bb238709869bc079bdc18e3bd4d
DESKTOP-N1D5NOF\User <kimdaesoop@gmail.com>
2018-09-27 18:04:05 +0900

    자동차 경주게임 step5

--- src/test/java/GameInfoTest.java
+++ src/test/java/GameInfoTest.java
@@ -1,46 +1,24 @@
-<<<<<<< HEAD
 import car.GameInfo;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import view.InputView;
-=======
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
->>>>>>> daesoop
 
 import static junit.framework.TestCase.assertEquals;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GameInfoTest {
     private GameInfo game;
-<<<<<<< HEAD
 
     @Before
     public void setup() {
         game = new GameInfo(InputView.name());
     }
 
-=======
-    @Before
-    public void setup() {
-        game = new GameInfo();
-    }
->>>>>>> daesoop
     @Test
     public void checkMove() {
         int n = game.checkMove(5);
         assertThat(n).isEqualTo(1);
-    }
-<<<<<<< HEAD
-
-=======
->>>>>>> daesoop
-    @Test
-    public void checkMove1() {
-        int n = game.checkMove(3);
-        assertThat(n).isEqualTo(0);
     }
 
     @After
